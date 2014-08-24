c331e894e35c780223995d5c0004f7e5ac2caed8
Kit <kittsville@googlemail.com>
2014-08-24 17:54:41 +0100

    Cleaned up admin-loans
    Removed deprecated code

--- wp-librarian-admin-loans.php
+++ wp-librarian-admin-loans.php
@@ -1,14 +1,3 @@
-<?php
-$action = $_GET['item_action']; // The action to be performed (e.g. loan an item)
-$page = $_GET['item_page']; // The requested page ( e.g. fine management page )
-$item_id = $_GET['item_id']; // The item's ID
-$member_id = $_GET['member_id']; // The ID of the member loaning/returning the item
-$fine_id = $_GET['fine_id']; // The ID of the fine being managed
-$start_date = $_GET['loan_start_date']; // When an item is to start being loaned
-$end_date = $_GET['loan_end_date']; // When a loan of an item should have ended by
-$length = $_GET['loan_length']; // The length of the loan
-
-?>
 <script>var GetVars = <?php echo json_encode( $_GET ); ?>;</script>
 <div class="wrap">
 <div id="title-wrap">
@@ -18,18 +7,7 @@ $length = $_GET['loan_length']; // The length of the loan
 </div>
 <!-- Filled with any notifications waiting in a session -->
 <div id="notifications-holder"></div>
-<div id="library-workspace">
-<strong>Loading...</strong>
-<?php
-	/* -- Date Sanitization -- */
-	/* Converts dates to Unix timestamps if specified */
-
-if ( isset( $start_date ) )
-	wp_lib_convert_date( $start_date );
-
-if ( isset( $end_date ) )
-	wp_lib_convert_date( $end_date );
-
-?>
-</div>
+	<div id="library-workspace">
+		<strong>Loading...</strong>
+	</div>
 </div>
