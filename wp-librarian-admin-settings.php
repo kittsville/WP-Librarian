kittsville/WP-Librarian

commit afe578cb0027e552da774b6e1ca6beb6374ceb33

Switched member header to new system
Moved some helper functions to helpers.php
Changed wp_lib_var_dump to make results easier to read
Added function to format loan management URL, as part of move towards a
useable loan management page
Removed pointlessly excessive code for queueing Dashboard styles
Fixed bug in Dashboard JS causing page to not redirect after a
successful action
Improved Dashboard template
Added new helper to format UNIX timestamp as abbr HTML element
Updated manage member to new dynamic page system
Added Dynatable to plugin repository, full integration will follow in
time

wp-librarian-helpers.php
@@ -27,6 +27,14 @@ function wp_lib_sanitize_item_cover( $raw ) {
 		return '';
 }
 
+// Santizes HTML POST data from a checkbox
+function wp_lib_sanitize_checkbox( $raw ) {
+	if ( $raw == 'true' )
+		return true;
+	else
+		return false;
+}
+
 	/* -- Data Validation Functions -- */
 
 // AJAX Wrapper for wp_lib_valid_item_id
@@ -182,7 +190,7 @@ function wp_lib_prefix_url( $option, $slug ) {
 	return $main_slug . '/' . $sub_slug;
 }
 
-// Formats a URL to manage the member with the given ID
+// Formats a URL to manage member with the given ID
 function wp_lib_format_manage_member( $member_id ) {
 	$args = array(
 		'dash_page'	=> 'manage-member',
@@ -192,7 +200,7 @@ function wp_lib_format_manage_member( $member_id ) {
 	return wp_lib_format_dash_url( $args );
 }
 
-// Formats a URL to manage the item with the given ID
+// Formats a URL to manage item with the given ID
 function wp_lib_format_manage_item( $item_id ) {
 	$args = array(
 		'dash_page'	=> 'manage-item',
@@ -202,11 +210,21 @@ function wp_lib_format_manage_item( $item_id ) {
 	return wp_lib_format_dash_url( $args );
 }
 
-// Formats a URL to manage the fine with the given ID
+// Formats a URL to manage fine with the given ID
 function wp_lib_format_manage_fine( $fine_id ) {
 	$args = array(
 		'dash_page'	=> 'manage-fine',
 		'fine_id'	=> $fine_id
+	);
+	
+	return wp_lib_format_dash_url( $args );
+}
+
+// Formats a URL to manage loan with the given ID
+function wp_lib_format_manage_loan( $loan_id ) {
+	$args = array(
+		'dash_page'	=> 'manage-loan',
+		'loan_id'	=> $loan_id
 	);
 	
 	return wp_lib_format_dash_url( $args );
@@ -252,10 +270,14 @@ function wp_lib_prep_date_column( $id, $key ) {
 	
 	// If date is valid returns formatted date
 	if ( is_numeric( $date ) )
-		return date( 'd-m-y', $date );
+		return wp_lib_format_unix_timestamp( $date );
 	// Otherwise return dash to show missing information
 	else
 		return '-';
+}
+
+function wp_lib_format_unix_timestamp( $timestamp ) {
+	return '<abbr title="' . date( 'Y/m/d g:i:s A', $timestamp ) . '">' . date( 'Y/m/d', $timestamp ) . '</abbr>';
 }
 
 // If a date is false, sets to current date
@@ -309,10 +331,6 @@ function wp_lib_format_money( $value ) {
 	/* -- AJAX -- */
 	/* Functions that assist preparing data for the client/server */
 
-// Given member object, returns rendered member management header
-function wp_lib_prep_member_management_header( $member ) {
-	return array();
-}
 
 // Given item ID, returns rendered item management header
 function wp_lib_prep_item_management_header( $item_id ) {
@@ -358,7 +376,7 @@ function wp_lib_prep_item_management_header( $item_id ) {
 	// Adds item status as last meta field
 	$meta_fields[] = array( 'Status', wp_lib_prep_item_available( $item_id, true ) );
 	
-	// Prepares management header
+	// Finalises and returns management header
 	return array(
 		array(
 			'type'		=> 'div',
@@ -402,7 +420,7 @@ function wp_lib_prep_fine_management_header( $fine_id ) {
 	if ( $title_array['deleted'] )
 		wp_lib_error( 205, array( 'item', $title_array['title'] ) );
 	
-	// Prepares management header
+	// Finalises and returns management header
 	return array(
 		array(
 			'type'		=> 'div',
@@ -412,6 +430,7 @@ function wp_lib_prep_fine_management_header( $fine_id ) {
 					'type'	=> 'metabox',
 					'title'	=> 'Details',
 					'fields'=> array(
+						array( 'Fine ID', $fine_id ),
 						array( 'Item', $title_array['title'] ),
 						array( 'Member', $member_array['name'] ),
 						array( 'Amount', $fine_formatted ),
@@ -424,29 +443,35 @@ function wp_lib_prep_fine_management_header( $fine_id ) {
 	);
 }
 
-// Renders the header of the member management page, displaying information about the member
-function wp_lib_render_member_management_header( $member_id ) {
-	// Fetches member object using member ID
-	$member = get_term( $member_id, 'wp_lib_member' );
+// Given member object, returns rendered member management header
+function wp_lib_prep_member_management_header( $member ) {
+	// Fetches member meta
+	$meta = wp_lib_fetch_member_meta( $member->term_id );
 	
-	?>
-	<h2>Managing: <?= $member->name ?></h2>
-	<strong>Member ID: </strong><?= $member->term_id ?><br />
-	<?php
-	// Sets up loan history parameters
-	$args = array(
-		'post_type' => 'wp_lib_loans',
-		'tax_query' => array(
-			array(
-				'taxonomy'	=> 'wp_lib_member',
-				'field'		=> 'term_id',
-				'terms'		=> $member_id
+	$fine_total = "£0.00";
+	
+	// Sets up header's meta fields
+	$meta_fields = array(
+		array( 'Member ID', $member->term_id ),
+		array( 'Phone', $meta['member_phone'] ),
+		array( 'Mobile', $meta['member_mobile'] ),
+		array( 'Total Fines', $fine_total )
+	);
+	
+	// Finalises and returns management header
+	return array(
+		array(
+			'type'		=> 'div',
+			'classes'	=> 'member-man',
+			'inner'		=> array(
+				array(
+					'type'	=> 'metabox',
+					'title'	=> 'Details',
+					'fields'=> $meta_fields
+				)
 			)
 		)
-	);	
-	
-	// Creates query of all loans attached to this member
-	$query = new WP_Query( $args );
+	);
 }
 
 // Creates option element for each member in the library, stored as an array
@@ -474,7 +499,48 @@ function wp_lib_prep_member_options() {
 	return $options;
 }
 
+	/* -- Debugging -- */
+
+// Dumps any number of given variables between <pre> tags
+function wp_lib_var_dump() {
+	// Gets all given params
+	$args = func_get_args();
+	
+	// For each param, var_dump between <pre> tags to format code properly in browser
+	foreach ( $args as $arg ){
+		echo '<pre style="background:white;">';
+		var_dump( $arg );
+		echo '</pre>';
+	}
+}
+
 	/* -- Miscellaneous -- */
+	
+// Given member ID, fetches meta associated with member
+function wp_lib_fetch_member_meta( $member_id, $meta_key = false ) {
+	// Fetches meta associated with member, stored as an option
+	$meta = get_option( 'wp_lib_tax_' . $member_id );
+	
+	// If no meta was found, return false
+	if ( !$meta )
+		return false;
+	
+	// If no specific key was specified, return all meta
+	if ( !$meta_key )
+		return $meta;
+	
+	// Provided key refers to existing member meta, return that specific meta
+	if ( array_key_exists( $meta_key, $meta ) )
+		return $meta[$meta_key];
+	else
+		return false;
+}
+
+// Given an array, updates member's meta
+function wp_lib_update_member_meta( $member_id, $meta ) {
+	// Updates option used to store member's meta with given array, returns success/failure
+	return update_option( 'wp_lib_tax_' . $member_id, $meta );
+}
 
 // Fetches member object given member ID
 function wp_lib_fetch_member( $member_id ) {
@@ -499,7 +565,6 @@ function wp_lib_fetch_member( $member_id ) {
 		return $member;
 	}
 }
-
 
 // Fetches member's name and, if needed, formats as a hyperlink. Uses archived member name on failure
 function wp_lib_fetch_member_name( $item_id, $hyperlink = true, $array = false, $ending = true ) {

wp-librarian.php
@@ -626,7 +626,7 @@ function wp_lib_process_member_meta_box( $member_id ) {
 	}
 	
 	// Saves meta array
-	update_option( "wp_lib_tax_{$member_id}", $meta_array );
+	wp_lib_update_member_meta( $member_id, $meta_array );
 }
 
 // Removes unneeded meta boxes from item editing page
@@ -652,9 +652,6 @@ add_action( 'admin_menu', function() {
 
 	// Registers Library Dashboard and saves handle to variable
 	$page = add_submenu_page('edit.php?post_type=wp_lib_items', 'Library Dashboard', 'Dashboard', 'edit_post', 'dashboard', 'wp_lib_render_dashboard');
-	
-	// Hooks Dashboard css loading to Dashboard page hook
-	add_action( 'admin_print_styles-' . $page, 'wp_lib_enqueue_dashboard_styles' );
 });
 
 	/* -- Settings API -- */
@@ -746,11 +743,6 @@ function wp_lib_render_settings() {
 	/* -- Scripts and Styles -- */
 	/* Registers and enqueues JavaScript and CSS files on the relevant pages, including their dependencies */
 
-// Enqueues CSS files used in the Library Dashboard
-function wp_lib_enqueue_dashboard_styles() {
-	wp_enqueue_style( 'wp_lib_dashboard' );
-}
-
 // Enqueues scripts and styles needed on different wp-admin pages
 add_action( 'admin_enqueue_scripts', function( $hook ) {
 	// Registers core JavaScript file for WP-Librarian, a collection of various essential functions
@@ -784,9 +776,11 @@ add_action( 'admin_enqueue_scripts', function( $hook ) {
 		// Library Dashboard
 		case 'wp_lib_items_page_dashboard':
 			wp_enqueue_script( 'wp_lib_dashboard', wp_lib_script_url( 'admin-dashboard' ), array( 'wp_lib_core' ), '0.1' );
-			wp_register_style( 'wp_lib_dashboard', wp_lib_style_url( 'admin-dashboard' ), array(), '0.1' );
-			wp_register_style( 'wp_lib_datepicker_mellon', wp_lib_style_url( 'mellon-datepicker' ), array(), '0.1' ); // Styles Datepickers
-			wp_register_style( 'wp_lib_datepicker', wp_lib_style_url( 'jquery-ui' ), array(), '1.10.1' ); // Core Datepickers Styles
+			wp_enqueue_script( 'dynatable', wp_lib_script_url( 'dynatable' ), array(), '0.3.1' );
+			wp_enqueue_style( 'wp_lib_dashboard', wp_lib_style_url( 'admin-dashboard' ), array(), '0.1' );
+			wp_enqueue_style( 'wp_lib_mellon-datepicker', wp_lib_style_url( 'mellon-datepicker' ), array(), '0.1' ); // Styles Datepicker
+			wp_enqueue_style( 'jquery-ui', wp_lib_style_url( 'jquery-ui' ), array(), '1.10.1' ); // Core Datepicker Styles
+			wp_enqueue_style( 'dynatable', wp_lib_style_url( 'dynatable' ), array( 'jquery-ui' ), '0.3.1' );
 		break;
 	}
 });
@@ -915,7 +909,7 @@ function wp_lib_member_edit_form_fields( $member ) {
 	
 	// Uses member ID to fetch any existing meta
 	// As taxonomies don't support meta, meta is stored as an option
-	$meta_array = get_option( "wp_lib_tax_{$member_id}" );
+	$meta_array = wp_lib_fetch_member_meta( $member_id );
 ?>
 	<tr class="form-field">
 		<th valign="top" scope="row">

css/dynatable.css
@@ -1,0 +1,69 @@
+/*
+ * jQuery Dynatable plugin 0.3.1
+ *
+ * Copyright (c) 2014 Steve Schwartz (JangoSteve)
+ *
+ * Dual licensed under the AGPL and Proprietary licenses:
+ *   http://www.dynatable.com/license/
+ *
+ * Date: Tue Jan 02 2014
+ */
+th a {
+  color: #fff;
+}
+th a:hover {
+  color: #fff;
+  text-decoration: underline;
+}
+
+.dynatable-search {
+  float: right;
+  margin-bottom: 10px;
+}
+
+.dynatable-pagination-links {
+  float: right;
+}
+
+.dynatable-record-count {
+  display: block;
+  padding: 5px 0;
+}
+
+.dynatable-pagination-links span,
+.dynatable-pagination-links li {
+  display: inline-block;
+}
+
+.dynatable-page-link,
+.dynatable-page-break {
+  display: block;
+  padding: 5px 7px;
+}
+
+.dynatable-page-link {
+  cursor: pointer;
+}
+
+.dynatable-active-page,
+.dynatable-disabled-page {
+  cursor: text;
+}
+.dynatable-active-page:hover,
+.dynatable-disabled-page:hover {
+  text-decoration: none;
+}
+
+.dynatable-active-page {
+  background: #71AF5A;
+  border-radius: 5px;
+  color: #fff;
+}
+.dynatable-active-page:hover {
+  color: #fff;
+}
+.dynatable-disabled-page,
+.dynatable-disabled-page:hover {
+  background: none;
+  color: #999;
+}

scripts/admin-core.js
@@ -3,7 +3,7 @@ var notificationCount = 0;
 
 jQuery( document ).ready(function($) {
 	// Selects the notification holder
-	window.notificationHolder = jQuery( '#notifications-holder' );
+	window.notificationHolder = jQuery( '#wp-lib-notifications' );
 });
 
 // Collects of a submitted form's parameters, removes the blank ones, then returns the result as an object

scripts/admin-dashboard.js
@@ -147,7 +147,10 @@ function wp_lib_load_page( page, ajaxData ) {
 		break;
 		
 		case undefined:
-			ajaxData['action'] = 'wp_lib_dashboard';
+			// If page is undefined, ajaxData will not exist and must be defined!
+			ajaxData = {
+				action	: 'wp_lib_dashboard'
+			}
 		break;
 		
 		default:
@@ -164,12 +167,16 @@ function wp_lib_load_page( page, ajaxData ) {
 			var ajaxResult = JSON.parse( response );
 		}
 		catch(e) {
-			wp_lib_local_error( "Failed to render data returned by server" );
+			wp_lib_local_error( "Server returned invalid JSON (possible server error/warning)" );
 			
 			// Debugging - Renders un-parseable returned data to workspace
-			jQuery('<pre/>', {
+			jQuery('<div/>', {
+				'style'	: 'background:grey;',
 				'html'	: response
-			}).appendTo( '#library-workspace' );
+			}).appendTo( '#wp-lib-workspace' );
+			
+			// Stops further execution of function
+			return false;
 		}
 		
 		// If server response was false, call error
@@ -220,7 +227,7 @@ function wp_lib_after_load() {
 	});
 	
 	// Adds listener for action performing buttons
-	jQuery('#library-workspace').on('click', '[name="dash_action"]', function ( e ){
+	jQuery('#wp-lib-workspace').on('click', '[name="dash_action"]', function ( e ){
 		// Fetches action to be performed
 		var action = e.currentTarget.value;
 		
@@ -235,7 +242,7 @@ function wp_lib_after_load() {
 	});
 	
 	// Adds listener for page loading buttons
-	jQuery('#library-workspace').on('click', '[name="dash_page"]', function ( e ){
+	jQuery('#wp-lib-workspace').on('click', '[name="dash_page"]', function ( e ){
 		// Fetches form parameters that have been set
 		var params = wp_lib_collect_form_params( '#library-form' );
 		
@@ -271,7 +278,7 @@ function wp_lib_render_page( pageArray ) {
 	}
 	
 	// Clears and selects Library workspace in preparation of new page
-	var libWorkspace = jQuery( '#library-workspace' ).empty();
+	var libWorkspace = jQuery( '#wp-lib-workspace' ).empty();
 	
 	// If page has any non-form content, render each element to the Library workspace
 	if ( pageArray.content ) {
@@ -535,6 +542,63 @@ function wp_lib_render_page( pageArray ) {
 				});
 			break;
 			
+			case 'dtable':
+				// If dev is an idiot and forgets to give the table a class, assigns one randomly
+				if (typeof( pageItem.id ) === 'undefined' ) {
+					elementObject.id = 'dynamic-table-' + Math.floor((Math.random() * 100) + 1);
+				}
+				
+				// Creates/selects base element
+				var theParent = $('<table/>', elementObject ).appendTo( theParent );
+				
+				// Adds head to table
+				var tableHead = $('<thead/>', {} ).appendTo( theParent );
+				
+				// Iterates through given table columns, adding them to the table head
+				$( pageItem.headers ).each( function( i, header ) {
+					$('<th/>', {
+						'html'	: header
+					} ).appendTo( tableHead );
+				});
+				
+				// Renders table body, to be dynamically populated
+				$('<tbody/>', {} ).appendTo( theParent );
+				
+				/* Prepares data for Dynatable */
+				
+				// Initialises output
+				var tableRecords = [];
+				
+				// Iterates through table data, adding it to output in correct format
+				$.each( pageItem.data, function( i, row ) {
+					// Initialises row output
+					var tableRow = {};
+					
+					// Iterates through row's data, setting up values for Dynatable
+					$.each( row, function( columnName, columnData ) {
+						// If row is a hyperlink, formats
+						if ( columnData instanceof Array ) {
+							columnData = '<a href="' + columnData[1] + '">' + columnData[0] + '</a>'; // Dynatable, Y U NO ACCEPT DOM OBJECTS?
+						}
+						
+						// Creates entry in table row named with column's slug (e.g. load-id) and sets value to 
+						tableRow[columnName] = columnData;
+					});
+					
+					// Adds row to table records
+					tableRecords.push( tableRow );
+				});
+				
+				//'#' + elementObject.id
+				
+				// Fills table with formatted data
+				$( '#' + elementObject.id ).dynatable({ 
+					dataset: {
+						records: tableRecords
+					}
+				});
+			break;
+			
 			default:
 				$('<strong/>', {
 					'html'	: 'UNKNOWN ELEMENT TYPE<br/>',
@@ -552,6 +616,13 @@ function wp_lib_format_tab_title( title ) {
 
 // Loads page content on page load. Only used if page is visited from a non-Dashboard page
 jQuery( document ).ready(function($) {
+	$.dynatableSetup({
+		table: {
+			headRowSelector: 'thead'
+		}
+	});
+
+
 	var GetVars = wp_lib_vars.getparams;
 	
 	// Removes default GET params as they are no longer needed

wp-librarian-ajax.php
@@ -537,17 +537,74 @@ function wp_lib_page_manage_member() {
 	
 	// Renders management header
 	$header = wp_lib_prep_member_management_header( $member );
-
+	
+	// Renders first part of member management page
 	$content[] = array(
 		'type'		=> 'paras',
 		'content'	=> array( 'Nothing much to see here yet!' )
 	);
 	
+	// Sets up loan history query arguments
+	$args = array(
+		'post_type' => 'wp_lib_loans',
+		'tax_query' => array(
+			array(
+				'taxonomy'	=> 'wp_lib_member',
+				'field'		=> 'term_id',
+				'terms'		=> $member->term_id
+			)
+		)
+	);
+	
+	// Creates query of all loans attached to this member
+	$loan_query = new WP_Query( $args );
+	
+	// Checks for any loans attached to member
+	if ( $loan_query->have_posts() ){
+		// Initialises loans array
+		$loans = array();
+		
+		// Iterates through loans
+		while ( $loan_query->have_posts() ) {
+			// Selects current post (loan)
+			$loan_query->the_post();
+			
+			// Fetches loan ID
+			$loan_id = get_the_ID();
+			
+			// Fetches all loan's meta
+			$meta = get_post_meta( $loan_id );
+			
+			// Gets item ID of loan
+			$item_id = $meta['wp_lib_item'][0];
+			
+			$loans[] = array(
+				'loan'		=> array( '#' . get_the_ID(), wp_lib_format_manage_loan( $loan_id ) ),
+				'item'		=> array( get_the_title( $item_id ), wp_lib_format_manage_item( $item_id ) ),
+				'startDate'	=> wp_lib_format_unix_timestamp( $meta['wp_lib_start_date'][0] ),
+				'endDate'	=> wp_lib_format_unix_timestamp( $meta['wp_lib_end_date'][0] )
+			);
+		}
+		
+		// Adds loans (rows) to table
+		$content[] = array(
+			'type'		=> 'dtable',
+			'id'		=> 'member-loans',
+			'headers'	=> array(
+				'Loan',
+				'Item',
+				'Start Date',
+				'End Date'
+			),
+			'data'		=> $loans
+		);
+	}
+
 	$page_title = 'Managing: ' . $member->name;
 	
 	$tab_title = 'Managing Member #' . $member_id;
 	
-	wp_lib_send_page( $page_title, $tab_title, $content );
+	wp_lib_send_page( $page_title, $tab_title, array_merge( $header, $content ) );
 }
 
 // Displays fine details and provides options to modify the fine

wp-librarian-dashboard-template.php
@@ -4,10 +4,6 @@
  * This file renders the template for the Dashboard.
  * The template is then populated and modified dynamically by JavaScript
  */
-
-wp_enqueue_style( 'wp_lib_datepicker' );
-wp_enqueue_style( 'wp_lib_datepicker_mellon' );
-
 ?>
 <div class="wrap">
 	<div id="title-wrap">
@@ -15,9 +11,6 @@ wp_enqueue_style( 'wp_lib_datepicker_mellon' );
 			<div id="page-title"></div>
 		</h2>
 	</div>
-	<!-- Filled with any notifications waiting in a session -->
-	<div id="notifications-holder"></div>
-	<div id="library-workspace">
-		<strong>Loading...</strong>
-	</div>
+	<div id="wp-lib-notifications"></div>
+	<div id="wp-lib-workspace"></div>
 </div>

commit 50ae1ea608042cb1a7596dac6b0e974a04e5b8ca

Minor Fixes
Fixed members being searchable on the front-end
Removed useless function wrapper
Fixed notifications not displaying on settings page, error introduced in
afe578cb0027e552da774b6e1ca6beb6374ceb33
Created helper to render notifications to page, to keep settings
template DRY

wp-librarian-admin-settings.php
@@ -22,22 +22,10 @@ if ( isset( $_GET['settings-updated'] ) ) {
 		wp_lib_flush_permalinks();
 		
 		// Notifies user settings have been updated
-		?>
-		<script type="text/javascript">
-			jQuery( document ).ready(function($) {
-				wp_lib_local_notification( 'Settings updated successfully' );
-			});
-		</script>
-		<?php
+		wp_lib_add_notification_on_load( 'Settings updated successfully' );
 	} elseif ( $updated == 'false' ) {
 		// Calls error to inform user that settings update failed
-		?>
-		<script type="text/javascript">
-			jQuery( document ).ready(function($) {
-				wp_lib_local_error( 'Settings failed to update' );
-			});
-		</script>
-		<?php
+		wp_lib_add_notification_on_load( 'Settings failed to update' );
 	}
 }
 ?>
@@ -46,7 +34,7 @@ if ( isset( $_GET['settings-updated'] ) ) {
 		<h2>WP-Librarian Settings</h2>
 	</div>
 	<!-- Filled with any notifications waiting in a session -->
-	<div id="notifications-holder"></div>
+	<div id="wp-lib-notifications"></div>
 
 	<div id="wp-lib-main-content">
 		<form method="POST" action="options.php">

wp-librarian-helpers.php
@@ -747,4 +747,14 @@ function wp_lib_prep_meta( $tax_array, $bold_name ) {
 	return $item_string . implode( get_option( 'wp_lib_taxonomy_spacer', ', ' ), $formatted_values );
 }
 
+// Renders notification to page, to send notification to cl
+function wp_lib_add_notification_on_load( $text ) {
+	?>
+	<script type="text/javascript">
+		jQuery( document ).ready(function($) {
+			wp_lib_local_notification( <?php echo json_encode( $text ); ?> );
+		});
+	</script>
+	<?php
+}
 ?>

wp-librarian.php
@@ -75,7 +75,7 @@ function wp_lib_register_post_and_tax() {
 			),
 			'public'				=> true,
 			'exclude_from_search'	=> true,
-			'publicly_queryable'	=> true,
+			'publicly_queryable'	=> false,
 			'show_in_menu' 			=> 'edit.php?post_type=wp_lib_items',
 			'supports'				=> array( 'title' ),
 			'rewrite'				=> array('slug' => get_option( 'wp_lib_members_slug', 'members' ) ),
@@ -619,7 +619,7 @@ add_action( 'admin_init', function () {
 		$slug['url'] = $site_url;
 		$slug['main'] = $main_slug;
 		add_settings_field( $slug['name'], $slug['title'], 'wp_lib_render_field_slug', 'wp_lib_items_page_wp-lib-settings', 'wp_lib_slug_group', $slug );
-		register_setting( 'wp_lib_slug_group', $slug['name'], 'wp_lib_sanitize_settings_slugs' );
+		register_setting( 'wp_lib_slug_group', $slug['name'], 'sanitize_title' );
 	}
 });
 
@@ -645,11 +645,6 @@ function wp_lib_render_field_slug( $args ) {
 	else
 		echo "{$args['url']}/<span class=\"wp_lib_main_slug-text\">{$args['main']}</span>/<span class=\"{$args['name']}-text\">{$current_slug}</span>/{$args['end']}/";
 	echo '</label>';
-}
-
-// Sanitizes updated slugs submitted from the plugin settings page
-function wp_lib_sanitize_settings_slugs( $slugs ) {
-	return sanitize_title( $slugs );
 }
 
 // Renders plugin settings page