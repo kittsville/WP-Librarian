kittsville/WP-Librarian

commit 9b6643f7a68c8a4a70af9dcb06c7178bfbe53e98

Multiple Dash Improvements
Added item loan list to scheduling and loan resolution pages
Added referrer to all Dash load_page requests, to shortly implement #18
Replaced use of static URL on 'view-items' to another Dash page with
dynamic page loading
Modified collect_form_params() to look for closest parent form element,
to allow 'Manage' buttons on 'view-items' page to work
Moved position of script loading for script dependant pages to bottom of
render_page()
Changed dynamic script handling, so the full script URL is constructed
clientside rather than serverside
Added logging of WP-Librarian errors that occur on Dash pages to the
console

scripts/admin-core.js
@@ -9,10 +9,10 @@ jQuery(function($){
 });
 
 // Collects of a submitted form's parameters, removes the blank ones, then returns the result as an object
-function wp_lib_collect_form_params() {
-
+function wp_lib_collect_form_params( clickedElement ) {
+	
 	// Fetches all form elements and creates an array of objects
-	var objects = jQuery( '#library-form' ).serializeArray();
+	var objects = jQuery( clickedElement ).closest('form').serializeArray();
 	
 	// Initialises results object
 	var result = {};
@@ -95,6 +95,17 @@ function wp_lib_render_notifications( notificationsArray ) {
 function wp_lib_render_notification( notificationText ) {
 	// Formats notification inside div and gives notification ID (to keep track of it)
 	var result = wp_lib_format_notification( notificationText );
+	
+	// Notification is an error and browser supports the browser console, logs to console as well
+	if ( notificationText[0] != 0 && typeof console !== 'undefined' ) {
+		if ( typeof console.error !== 'undefined' ) {
+			// If console.error is supported, report error as error
+			console.error( result[2] );
+		} else {
+			// Else report error using regular console.log entry
+			console.log( result[2] );
+		}
+	}
 
 	// Adds notification to the notification holder
 	notificationHolder.append( result[1] ).hide().fadeIn( 500 );
@@ -126,7 +137,7 @@ function wp_lib_format_notification( notification ) {
 	// Initialises variables
 	var classes = uID + ' ';
 	var message = '';
-	var onClick = " onclick='wp_lib_hide_notification(this)";
+	var consoleMessage = '';
 	
 	// If notification has no error code (defaulted to 0 )
 	if ( notification[0] == 0 ) {
@@ -136,15 +147,17 @@ function wp_lib_format_notification( notification ) {
 	} else if ( notification[0] == 1 ) {
 		// Uses error classes, which displays a red flared box
 		classes += 'wp-lib-error error';
-		message = "<strong style='color: red;'>WP-Librarian Error: " + notification[1] + "</strong>";
+		consoleMessage = 'WP-Librarian Error: ' + notification[1];
+		message = '<strong style="color: red;">' + consoleMessage + '</strong>';
 	} else {
 		// Uses error classes, which displays a red flared box
 		classes += 'wp-lib-error error';
-		message = "<strong style='color: red;'>WP-Librarian Error " + notification[0] + ": " + notification[1] + "</strong>";
+		consoleMessage = 'WP-Librarian Error ' + notification[0] + ': ' + notification[1];
+		message = '<strong style="color: red;">' + consoleMessage + '</strong>';
 	}
 	
 	// Returns HTML formatted notification
-	return [ uID, "<div onclick='wp_lib_hide_notification(this)' class='" + classes + "'><p>" + message + "</p></div>" ];
+	return [ uID, '<div onclick="wp_lib_hide_notification(this)" class="' + classes + '"><p>' + message + '</p></div>', consoleMessage ];
 }
 
 // Hides then deletes notification, called when notification is clicked or some time after it appeared

scripts/admin-dashboard.js
@@ -108,6 +108,11 @@ function wp_lib_load_page( ajaxData, stateLoad ) {
 	// Sets action, which specifies the name of the hook it will have in WordPress ( wp_ajax_{$hook} )
 	ajaxData.action = 'wp_lib_page';
 	
+	// Adds referrer to AJAX request, to allow page elements to be selectively loaded
+	if ( wp_lib_vars.hasOwnProperty( 'dash_page' ) ) {
+		ajaxData.ref = wp_lib_vars.dash_page;
+	}
+	
 	// Sends AJAX page request with given params, fills workspace div with response
 	jQuery.post( ajaxurl, ajaxData )
 	.done( function( response ) {
@@ -131,11 +136,15 @@ function wp_lib_load_page( ajaxData, stateLoad ) {
 			// Renders page using AJAX returned data
 			wp_lib_render_page( ajaxResult );
 			
+			// Pushes current dash page to wp_lib_vars
+			wp_lib_vars.dash_page = ajaxData.dash_page;
+			
 			// Checks if new page is replacing existing dynamically loaded page
 			// If it isn't then there's no need to add a new history entry
 			if ( stateLoad != 'history' ) {
-				// Deletes redundant parameter
+				// Deletes redundant parameters
 				delete ajaxData.action;
+				delete ajaxData.ref;
 				
 				// Deletes dash page if it's redundant
 				if ( ajaxData.dash_page == 'dashboard' ) {
@@ -203,16 +212,6 @@ function wp_lib_render_page( pageArray ) {
 	// Changes title of browser tab
 	document.title = wp_lib_format_tab_title( pageArray.title );
 	
-	// If any scripts are required by the page, loads them
-	if ( pageArray.hasOwnProperty('scripts') && pageArray.scripts instanceof Array ) {
-		$( pageArray.scripts ).each( function( i, scriptURL ) {
-			jQuery.getScript( scriptURL )
-			.fail( function( jqxhr, settings, exception ) {
-				wp_lib_local_error( "Failed to load JavaScript needed for this page" );
-			});
-		});
-	}
-	
 	// Clears and selects Library workspace in preparation of new page
 	var libWorkspace = jQuery( '#wp-lib-workspace' ).empty();
 	
@@ -259,6 +258,16 @@ function wp_lib_render_page( pageArray ) {
 		// Iterates over all given tables, rendering them inside the wrapper
 		jQuery( pageArray.content.table ).each( function( i, e ) {
 			render_page_element( e, tableWrap );
+		});
+	}
+	
+	// If any scripts are required by the page, loads them
+	if ( pageArray.hasOwnProperty('scripts') && pageArray.scripts instanceof Array ) {
+		$( pageArray.scripts ).each( function( i, scriptURL ) {
+			jQuery.getScript( wp_lib_vars.pluginsUrl + '/scripts/' + scriptURL + '.js' )
+			.fail( function( jqxhr, settings, exception ) {
+				wp_lib_local_error( "Failed to load JavaScript needed for this page" );
+			});
 		});
 	}
 	
@@ -727,16 +736,32 @@ function wp_lib_render_page( pageArray ) {
 						'html'	: listItemElements
 					});
 					
-					// Creates 'Manage' and 'View' buttons to loan/return item or view its public listing
-					[ [record.manage,'Manage','item-manage'], [record.view,'View','item-view'] ].forEach(function(itemButton,i) {
-						render_page_element( {
-							'type'		: 'button',
-							'link'		: 'url',
-							'href'		: itemButton[0],
-							'html'		: itemButton[1],
-							'classes'	: itemButton[2]
-						}, localParent );
-					});
+					// Renders manage item button to invisible form containing item's ID
+					render_page_element( {
+							type	: 'button',
+							link	: 'page',
+							value	: 'manage-item',
+							html	: 'Manage',
+							classes	: 'item-manage'
+						},
+						$('<form/>',{ // GGGG
+							html	:
+								$('<input/>',{
+									type	: 'hidden',
+									name	: 'item_id',
+									value	: record.item_id
+								})
+						}).appendTo( localParent )
+					);
+					
+					// Creates 'View' button to view items public listing
+					render_page_element( {
+						type		: 'button',
+						link		: 'url',
+						href		: record.view,
+						html		: 'View',
+						classes		: 'item-view'
+					}, localParent );
 					
 					// Converts to html string, as Dynatable doesn't accept DOM objects
 					return localParent.prop('outerHTML');
@@ -825,7 +850,7 @@ jQuery(function($){
 		var action = e.currentTarget.value;
 		
 		// Fetches form parameters that have been set
-		var params = wp_lib_collect_form_params();
+		var params = wp_lib_collect_form_params(e.originalEvent.explicitOriginalTarget);
 		
 		// Performs action
 		wp_lib_do_action( action, params );
@@ -834,7 +859,7 @@ jQuery(function($){
 	// Adds listener for page loading buttons
 	jQuery('#wp-lib-workspace').on('click', '[name="dash_page"]', function ( e ){
 		// Fetches form parameters that have been set
-		var params = wp_lib_collect_form_params();
+		var params = wp_lib_collect_form_params(e.originalEvent.explicitOriginalTarget);
 		
 		// Fetches page to be loaded
 		params.dash_page = e.currentTarget.value;

wp-librarian-ajax.php
@@ -635,7 +635,8 @@ function wp_lib_page_view_items() {
 				$item['authors'] = false;
 			}
 			
-			$item['manage'] = wp_lib_manage_item_url( $item_id );
+			$item['item_id'] = $item_id;
+			
 			$item['view'] = get_permalink();
 			
 			// Adds prepared item to array of all items
@@ -790,81 +791,8 @@ function wp_lib_page_manage_item() {
 		);
 	}
 	
-	// Sets up loan history query arguments
-	$args = array(
-		'post_type' 	=> 'wp_lib_loans',
-		'post_status'	=> 'publish',
-		'meta_query'	=> array(
-			array(
-				'key'		=> 'wp_lib_item',
-				'value'		=> $item_id,
-				'compare'	=> 'IN'
-			)
-		)
-	);
-	
-	// Creates query of all loans of this item
-	$loan_query = new WP_Query( $args );
-	
-	// Checks for any loans in query
-	if ( $loan_query->have_posts() ){
-		// Initialises loans array
-		$loans = array();
-		
-		// Iterates through loans
-		while ( $loan_query->have_posts() ) {
-			// Selects current post (loan)
-			$loan_query->the_post();
-			
-			// Fetches loan ID
-			$loan_id = get_the_ID();
-			
-			// Fetches all loan's meta
-			$meta = get_post_meta( $loan_id );
-			
-			// Gets member ID from loan meta
-			$member_id = $meta['wp_lib_member'][0];
-			
-			$loan_status = wp_lib_format_loan_status( $meta['wp_lib_status'][0] );
-			
-			// If loan incurred fine, change loan status to include a link to manage said fine
-			if ( $meta['wp_lib_status'][0] == 4 ) {
-				$loan_status = array( $loan_status, wp_lib_manage_fine_url( $meta['wp_lib_fine'][0] ) );
-			}
-			
-			$loans[] = array(
-				'loan'		=> array( '#' . get_the_ID(), wp_lib_manage_loan_url( $loan_id ) ),
-				'member'	=> array( get_the_title( $member_id ), wp_lib_manage_member_url( $member_id ) ),
-				'status'	=> $loan_status,
-				'loaned'	=> wp_lib_format_unix_timestamp( $meta['wp_lib_start_date'][0] ),
-				'expected'	=> wp_lib_format_unix_timestamp( $meta['wp_lib_end_date'][0] ),
-				'returned'	=> wp_lib_format_unix_timestamp( $meta['wp_lib_returned_date'][0] )
-			);
-		}
-		
-		// Adds loans (rows) to table
-		$table[] = array(
-			'type'		=> 'dtable',
-			'id'		=> 'member-loans',
-			'headers'	=> array(
-				'Loan',
-				'Member',
-				'Status',
-				'Loaned',
-				'Expected',
-				'Returned'
-			),
-			'data'		=> $loans,
-			'labels'	=> array(
-				'records'	=> 'loans'
-			)
-		);
-	} else {
-		$table[] = array(
-			'type'		=> 'paras',
-			'content'	=> array( 'No loans to display' )
-		);
-	}
+	// Fetches list of loans of item
+	$table = wp_lib_prep_loans_table( $item_id );
 	
 	// Encodes page as an array to be rendered client-side
 	wp_lib_send_page( 'Managing: ' . get_the_title( $item_id ), 'Managing Item #' . $item_id, $header, $form, $table );
@@ -1082,7 +1010,7 @@ function wp_lib_page_manage_fine() {
 // Page for looking up an item by its barcode
 function wp_lib_page_scan_item() {
 	// Enqueues barcode page script
-	$scripts[] = wp_lib_script_url( 'admin-barcode-scanner' );
+	$scripts[] = 'admin-barcode-scanner';
 	
 	$form = array(
 		wp_lib_prep_nonce( 'Lookup Item Barcode' ),
@@ -1200,7 +1128,10 @@ function wp_lib_page_scheduling_page() {
 		)
 	);
 	
-	wp_lib_send_page( 'Scheduling loan of ' . get_the_title( $item_id ), 'Scheduling loan of #' . $item_id, $header, $form );
+	// Fetches list of loans of item
+	$table = wp_lib_prep_loans_table( $item_id );
+	
+	wp_lib_send_page( 'Scheduling loan of ' . get_the_title( $item_id ), 'Scheduling loan of #' . $item_id, $header, $form, $table );
 }
 
 // Displays page for returning an item in the past
@@ -1324,11 +1255,14 @@ function wp_lib_page_resolution_page() {
 		)
 	);
 	
+	// Fetches list of loans of item
+	$table = wp_lib_prep_loans_table( $item_id );
+	
 	$page_title = 'Resolving Late Item: ' . $title;
 	
 	$tab_title = 'Resolving Item #' . $item_id;
 	
-	wp_lib_send_page( $page_title, $tab_title, $header, $form );
+	wp_lib_send_page( $page_title, $tab_title, $header, $form, $table );
 }
 
 // Allows Librarian to reduce money owed by a member for late returns

wp-librarian-helpers.php
@@ -616,6 +616,86 @@ function wp_lib_prep_member_options( $default_option = true ) {
 	return $options;
 }
 
+function wp_lib_prep_loans_table( $item_id ) {
+	// Sets up loan history query arguments
+	$args = array(
+		'post_type' 	=> 'wp_lib_loans',
+		'post_status'	=> 'publish',
+		'meta_query'	=> array(
+			array(
+				'key'		=> 'wp_lib_item',
+				'value'		=> $item_id,
+				'compare'	=> 'IN'
+			)
+		)
+	);
+	
+	// Creates query of all loans of this item
+	$loan_query = new WP_Query( $args );
+	
+	// Checks for any loans in query
+	if ( $loan_query->have_posts() ){
+		// Initialises loans array
+		$loans = array();
+		
+		// Iterates through loans
+		while ( $loan_query->have_posts() ) {
+			// Selects current post (loan)
+			$loan_query->the_post();
+			
+			// Fetches loan ID
+			$loan_id = get_the_ID();
+			
+			// Fetches all loan's meta
+			$meta = get_post_meta( $loan_id );
+			
+			// Gets member ID from loan meta
+			$member_id = $meta['wp_lib_member'][0];
+			
+			$loan_status = wp_lib_format_loan_status( $meta['wp_lib_status'][0] );
+			
+			// If loan incurred fine, change loan status to include a link to manage said fine
+			if ( $meta['wp_lib_status'][0] == 4 ) {
+				$loan_status = array( $loan_status, wp_lib_manage_fine_url( $meta['wp_lib_fine'][0] ) );
+			}
+			
+			$loans[] = array(
+				'loan'		=> array( '#' . get_the_ID(), wp_lib_manage_loan_url( $loan_id ) ),
+				'member'	=> array( get_the_title( $member_id ), wp_lib_manage_member_url( $member_id ) ),
+				'status'	=> $loan_status,
+				'loaned'	=> wp_lib_format_unix_timestamp( $meta['wp_lib_start_date'][0] ),
+				'expected'	=> wp_lib_format_unix_timestamp( $meta['wp_lib_end_date'][0] ),
+				'returned'	=> wp_lib_format_unix_timestamp( $meta['wp_lib_returned_date'][0] )
+			);
+		}
+		
+		// Adds loans (rows) to table
+		$table[] = array(
+			'type'		=> 'dtable',
+			'id'		=> 'member-loans',
+			'headers'	=> array(
+				'Loan',
+				'Member',
+				'Status',
+				'Loaned',
+				'Expected',
+				'Returned'
+			),
+			'data'		=> $loans,
+			'labels'	=> array(
+				'records'	=> 'loans'
+			)
+		);
+	} else {
+		$table[] = array(
+			'type'		=> 'paras',
+			'content'	=> array( 'No loans to display' )
+		);
+	}
+	
+	return $table;
+}
+
 	/* -- Debugging -- */
 
 // Renders current plugin's version, update channel and similar information

commit 7e85e70ec696df6e0d9712b4a726449e22a777b5

Dash Logging/Notifications Improvements
Added notification checking after failed JSON parsing by load_page()
Fixed duplicate logging of errors by JSON parsing/AJAX failure reporting
caused by 9b6643f7a68c8a4a70af9dcb06c7178bfbe53e98
Replaced use of console.error with console.log as it was being used
outside of its purpose

scripts/admin-core.js
@@ -28,8 +28,7 @@ function wp_lib_collect_form_params( clickedElement ) {
 
 // General purpose AJAX failed message, probably a network issue, unless the server is hosted off some terrible cloud server
 function wp_lib_ajax_fail() {
-	console.log( "WP-Librarian: AJAX Request failed" );
-	wp_lib_local_error( "Unable to complete request. The website might be down or you may be having connection issues." );
+	wp_lib_local_error( "Unable to contact website. It might be down or you may be having connection issues." );
 }
 
 // Adds notification to client-side buffer
@@ -98,13 +97,7 @@ function wp_lib_render_notification( notificationText ) {
 	
 	// Notification is an error and browser supports the browser console, logs to console as well
 	if ( notificationText[0] != 0 && typeof console !== 'undefined' ) {
-		if ( typeof console.error !== 'undefined' ) {
-			// If console.error is supported, report error as error
-			console.error( result[2] );
-		} else {
-			// Else report error using regular console.log entry
-			console.log( result[2] );
-		}
+		console.log( result[2] );
 	}
 
 	// Adds notification to the notification holder
@@ -184,7 +177,6 @@ function wp_lib_parse_json( rawJSON ) {
 		var parsedJSON = JSON.parse( rawJSON );
 	}
 	catch(e) {
-		console.log( "WP-Librarian: Server returned invalid JSON" );
 		wp_lib_local_error( "Unable to complete request. Server returned invalid response" );
 		
 		// Debugging - Renders un-parseable returned data to workspace

scripts/admin-dashboard.js
@@ -126,6 +126,9 @@ function wp_lib_load_page( ajaxData, stateLoad ) {
 		
 		// If page load failed, stops function execution, see wp_lib_parse_json
 		if ( ajaxResult === 0 ) {
+			// Fetches and renders any new notifications, as one have been generated in the process of the server failing to return a valid response
+			wp_lib_display_notifications();
+			
 			return false;
 		}
 		