kittsville/WP-Librarian

commit 5444d418619bd4d643df64d06534561894b2cfe8

Changed error()'s behaviour on known error codes
Change error()'s behaviour. If the error code is not listed it will call
an error using its parents original $die behaviour, rather than always
calling die().

wp-librarian-functions.php
@@ -598,7 +598,7 @@ function wp_lib_cancel_fine( $fine_id ) {
 function wp_lib_error( $error_id, $die = false, $param = 'NULL' ) {
 	// Checks if error code is valid and error exists, if not returns error
 	if ( !is_numeric( $error_id ) )
-		wp_lib_error( 901, true );
+		wp_lib_error( 901, $die );
 	
 	// Array of all error codes and their explanations
 	//   0 - Reserved, see wp_lib_add_notification()
@@ -669,7 +669,7 @@ function wp_lib_error( $error_id, $die = false, $param = 'NULL' ) {
 	
 	// Checks if error exists, if not returns error
 	if ( !array_key_exists( $error_id, $all_errors ) )
-		wp_lib_error( 902, true );
+		wp_lib_error( 902, $die );
 	
 	// Fetches error explanation from array
 	$error_text = $all_errors[$error_id];

commit 1c9b7dd4d634c6f79e604d4af23019f38220dff3

Fixed bug in error processing
Fixed bug introduced in 5444d418619bd4d643df64d06534561894b2cfe8 causing
error() not to stop having detected an invalid error code if called by
AJAX

wp-librarian-functions.php
@@ -597,8 +597,10 @@ function wp_lib_cancel_fine( $fine_id ) {
 // Returns explanation of error given error code
 function wp_lib_error( $error_id, $die = false, $param = 'NULL' ) {
 	// Checks if error code is valid and error exists, if not returns error
-	if ( !is_numeric( $error_id ) )
+	if ( !is_numeric( $error_id ) ) {
 		wp_lib_error( 901, $die );
+		return;
+	}
 	
 	// Array of all error codes and their explanations
 	//   0 - Reserved, see wp_lib_add_notification()
@@ -664,12 +666,14 @@ function wp_lib_error( $error_id, $die = false, $param = 'NULL' ) {
 		600	=> 'Unable to schedule debugging loan',
 		601	=> 'Unable to fulfil successfully scheduled debugging loan',
 		901 => 'Error encountered while processing error (error code not a number)',
-		902 => 'Error encountered while processing error (error does not exist)'
+		902 => "Error encountered while processing error ID:{$param} (error does not exist)"
 	);
 	
 	// Checks if error exists, if not returns error
-	if ( !array_key_exists( $error_id, $all_errors ) )
-		wp_lib_error( 902, $die );
+	if ( !array_key_exists( $error_id, $all_errors ) ) {
+		wp_lib_error( 902, $die, $error_id );
+		return;
+	}
 	
 	// Fetches error explanation from array
 	$error_text = $all_errors[$error_id];