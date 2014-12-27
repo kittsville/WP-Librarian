kittsville/WP-Librarian

commit 4cc580d8ddc08973603f826a2ad5d5efe0891ae8

Implemented class based Settings
Created class to handle registering settings/sections and fields
Changed siteUrl in admin-core variable localization to use WP function
site_url()
Added function that loads helper classes
Added functions to handle storage of booleans in settings
Changed all handling of plugin settings to store values as an array
Other related changes to setting handling

wp-librarian-ajax.php
@@ -239,7 +239,7 @@ add_action( 'wp_ajax_wp_lib_action', function() {
 			$member_id = get_post_meta( $loan_id, 'wp_lib_member', true );
 			
 			// If item is successfully loaned
-			if ( wp_lib_give_item( $item_id, $loan_id, $member_id, $give_date ) ) {
+			if ( wp_lib_give_item( $loan_id, $give_date ) ) {
 				// Inform user of success via notification
 				wp_lib_add_notification( get_the_title( $item_id ) . ' has been loaned to ' . get_the_title( $member_id ) );
 				
@@ -256,8 +256,11 @@ add_action( 'wp_ajax_wp_lib_action', function() {
 			// Fetches params from AJAX request
 			$item_id = $_POST['item_id'];
 			
+			// Member ID, currently manually set. Will use debugging user in future
+			$member_id = 6;
+			
 			// If item ID fails to validate or debugging mode isn't on, stop action
-			if ( !wp_lib_valid_item_id( $item_id ) || WP_LIB_DEBUGGING_MODE !== true )
+			if ( !wp_lib_valid_item_id( $item_id ) || WP_LIB_DEBUG_MODE !== true )
 				wp_lib_stop_ajax( false );
 			
 			// Validates Nonce
@@ -267,13 +270,13 @@ add_action( 'wp_ajax_wp_lib_action', function() {
 			$end_date = current_time( 'timestamp' ) - ( 3 * 24 * 60 * 60 );
 			
 			// If possible creates loan of item starting 10 days ago, due 3 days ago (to test fining capabilities)
-			$loan_id = wp_lib_schedule_loan( $item_id, '127', $start_date, $end_date );
+			$loan_id = wp_lib_schedule_loan( $item_id, $member_id, $start_date, $end_date );
 			
 			if ( !is_numeric( $loan_id ) )
 				wp_lib_stop_ajax( false, 600 );
 			
 			// Fulfils scheduled loan, giving item 900 seconds after the loan was scheduled to start
-			if ( wp_lib_give_item( $item_id, $loan_id, '127', current_time( 'timestamp' ) - ( 10 * 24 * 60 * 60 ) + 900 ) ) {
+			if ( wp_lib_give_item( $loan_id, current_time( 'timestamp' ) - ( 10 * 24 * 60 * 60 ) + 900 ) ) {
 				wp_lib_add_notification( "Debugging loan created!" );
 				wp_lib_stop_ajax( true );
 			} else {
@@ -1484,7 +1487,7 @@ function wp_lib_page_resolution_page() {
 	// Item's title
 	$title = get_the_title( $item_id );
 	// Librarian set charge for each day an item is late
-	$fine_per_day = get_option( 'wp_lib_fine_daily' );
+	$fine_per_day = get_option( 'wp_lib_fine_daily', array(0) )[0];
 	// Days item is late
 	$late = -wp_lib_cherry_pie( $loan_id, $date );
 	// Total fine member member is facing, if charged

css/admin-settings.css
@@ -6,6 +6,10 @@
  * Version:		0.1
  */
 
-.slug-label {
-	margin-left: 6px;
+label.slug-label {
+	margin-left: 5px;
+	padding: 3px;
+	border-radius: 3px;
+    overflow: hidden;
+}
 }

helpers/settings.php
@@ -1,0 +1,206 @@
+<?php
+$section = array(
+	'name'		=> 'wp_lib_library_group',
+	'title'		=> 'General Settings',
+	'callback'	=> false,
+	'page'		=> 'wp_lib_library_group-options',
+	'settings'	=> array(
+		array(
+			'name'			=> 'wp_lib_slugs',
+			'sanitize'		=> function($raw){/*Function that sanitizes settings' fields */},
+			'field_type'	=> 'textField', // Field callback can be defined at setting or field level
+			'fields'		=> array(
+				array(
+					'name'			=> 'Main Slug',
+					'args'			=> array(
+						'filter'	=> function( $raw ) {}, // Filters input before field is rendered
+						'position'	=> 0, // Position of field value in options array
+						'alt'		=> 'This forms the base of all public Library pages',
+						'class'		=> 'main-slug'
+					)
+				)
+			)
+		),
+	)
+);
+class WP_LIB_SETTINGS {
+	function __construct( $section ) {
+		// If header to render section description is not provided, passes dummy callback
+		if ( !isset( $section['callback'] ) )
+			$section['callback'] = false;
+		
+		// Registers setting section
+		add_settings_section( $section['name'], $section['title'], $section['callback'], $section['page'] );
+		
+		// Iterates over settings, generating fields for each settings' fields 
+		foreach( $section['settings'] as $setting ) {
+			// Registers setting section's page
+			// Uses sanitization callback specific to current setting, if one exists, defaults to section's sanitization callback
+			register_setting( $section['name'], $setting['name'], ( isset( $setting['sanitize'] ) ? $setting['sanitize'] : $section['sanitize'] ) );
+			
+			// If undefined, initialises setting level classes
+			if ( !isset( $setting['classes'] ) )
+				$setting['classes'] = array();
+			
+			// Iterates over setting's fields, registering them
+			foreach ( $setting['fields'] as $position => $field ) {
+				// Initialises field args, if necessary
+				if ( !isset( $field['args'] ) )
+					$field['args'] = array();
+				
+				// Adds setting name to arguments to be passed to field rendering callback
+				$field['args']['setting_name'] = $setting['name'];
+				
+				// Iterates over field parameters which can be defined at a setting or field level, applying setting level prams to fields
+				// Setting level param will only be inherited if the field hasn't its own specified param
+				foreach ( ['field_type'] as $param ) {
+					if ( !isset($field[$param]) && isset( $setting[$param] ) )
+						$field[$param] = $setting[$param];
+				}
+				
+				// Iterates over field args which can be defined at a setting or field level, applying setting level prams to fields
+				foreach ( ['html_filter'] as $arg ) {
+					if ( isset( $setting[$arg] ) )
+						$field['args'][$arg] = $setting[$arg];
+				}
+				
+				// If undefined, initialises field level classes
+				if ( !isset( $field['args']['classes'] ) )
+					$field['args']['classes'] = array();
+				
+				// Merges parent (setting) classes into child (field)
+				$field['args']['classes'] = array_merge( $field['args']['classes'], $setting['classes'] );
+				
+				// Prepares callback to render field for use by WordPress by adding class name
+				$field['field_type'] = array( $this, $field['field_type'] );
+				
+				// Adds field position in setting array to field args
+				$field['args']['position'] = $position;
+				
+				// Registers setting field to setting
+				// Passes callback to render field using specific callback for this field, if one exists. Falls back to setting's field rendering callback
+				add_settings_field( $setting['name'] . '[' . $position . ']' , $field['name'], $field['field_type'], $section['page'], $section['name'], $field['args'] );
+			}
+		}
+	}
+	
+	private function addDescription( &$output, $args ) {
+		if ( isset( $args['alt'] ) )
+			$output[] = '<p class="tooltip description">' . $args['alt'] . '</p>';
+	}
+	
+	// Fetches option and applies any given filters to it
+	private function getOption( $args ) {
+		// Fetches option value from database. Uses false if option does not exist
+		$option = get_option( $args['setting_name'], false );
+		
+		// If option does not exist or is invalid, call error. This will be handled better in future
+		if ( !is_array( $option ) )
+			wp_lib_error( 114, true );
+		
+		// Fetches field value from option array
+		if ( isset( $option[$args['position']] ) )
+			$option = $option[$args['position']];
+		else
+			wp_lib_error( 115, true );
+		
+		// If filter exists, to prep the option for field display, filters
+		if ( isset( $args['filter'] ) )
+			$option = $args['filter']( $option, $args );
+		
+		return $option;
+	}
+	
+	// Sets up field's element's properties
+	private function setupFieldProperties( $args, $add_prop ) {
+		// Merges given field classes with default field classes
+		$classes = array_merge(
+			$args['classes'],
+			array(
+				'setting-' . $args['setting_name']
+			)
+		);
+		
+		// Merges additional field properties with default field properties
+		$prop_array = array_merge(
+			$add_prop,
+			array(
+				'class'	=> implode( ' ', $classes ),
+				'name'	=> $args['setting_name'] . '[' . $args['position'] . ']',
+				'id'	=> $args['setting_name'] . '[' . $args['position'] . ']'
+			)
+		);
+		
+		// Initialises element properties
+		$properties = '';
+		
+		// Iterates over properties, formatting html element properties as a string
+		foreach( $prop_array as $key => $value ) {
+			$properties .= $key . '="' . $value . '" ';
+		}
+		
+		return $properties;
+	}
+	
+	public function textInput( $args ) {
+		$properties = array(
+			'type' => 'text',
+			'value'=> $this->getOption( $args )
+		);
+		
+		// Sets field output
+		$output = array(
+			'<input ' . $this->setupFieldProperties( $args, $properties ) . '/>'
+		);
+		
+		// Adds field description, if one exists
+		$this->addDescription( $output, $args );
+		
+		// If hook exists to add html elements to the output, apply
+		if ( isset( $args['html_filter'] ) )
+			$output = $args['html_filter']( $output, $args );
+		
+		// Renders output to setting field
+		$this->outputLines( $output );
+	}
+	
+	
+	public function checkboxInput( $args ) {
+		$properties = array(
+			'type'	=> 'checkbox',
+			'value'	=> 3
+		);
+		
+		if ( $this->getOption( $args ) == 3 )
+			$properties['checked'] = 'checked';
+		
+		// Sets field output
+		$output = array(
+			'<input ' . $this->setupFieldProperties( $args, $properties ) . '/>'
+		);
+		
+		// Adds field description, if one exists
+		$this->addDescription( $output, $args );
+		
+		// If hook exists to add html elements to the output, apply
+		if ( isset( $args['html_filter'] ) )
+			$output = $args['html_filter']( $output, $args );
+		
+		// Renders output to setting field
+		$this->outputLines( $output );
+	}
+	
+	private function outputLines( $lines ) {
+		foreach ( $lines as $line ) {
+			echo $line;
+		}
+	}
+}
+
+
+
+
+
+
+
+?>

scripts/admin-settings.js
@@ -1,19 +1,31 @@
-jQuery( document ).ready(function($) {
+jQuery(function($){
 	// Renders any buffered notifications
 	wp_lib_display_notifications();
 	
-	/* 
-	 * Updates example slug as text input is updated, providing live preview of what the slug would look like
-	 * var text is the input box where the user types the slug
-	 * var textPreview is an html span that updates to show whatever var text currently is
-	 */
-	$('.slug-input').each(function(index,element){
-		var text = $(element);
-		text.on( 'input', function(e) {
-			var textID = $(element).attr("id");
-			textPreview = $( '.' + textID + '-text' );
-			textPreview.text( string_to_slug( text.val() ) );
-			
+	// Updates slug previews based on changed input field
+	function wp_lib_update_slug_previews( slugInput ) {
+		// Fetches and sanitizes current slug input
+		var inputValue = string_to_slug(slugInput.val());
+		
+		// If slug input is main slug, update all previews that use the main slug
+		// Otherwise updates only preview for that input
+		if ( slugInput.attr('name') === 'wp_lib_slugs[0]' ) {
+			$('span[name="main-slug-text"]').each(function(i,element){
+				$(element).text(inputValue);
+			});
+		} else {
+			slugInput.next().find('span.slug-preview').text(inputValue);
+		}
+	}
+	
+	// Iterates over slug input fields, setting up previews and hooking preview updating to input change
+	$('input.slug-input').each(function(index,element){
+		var slugInput = $(element);
+		
+		wp_lib_update_slug_previews( slugInput );
+		
+		slugInput.on( 'input', function(e) {
+			wp_lib_update_slug_previews( slugInput );
 		});
 	});
 });

wp-librarian-admin-settings.php
@@ -32,11 +32,15 @@ if ( isset( $_GET['settings-updated'] ) ) {
 // Sets settings tab to display based on GET param
 switch ( $_GET['tab'] ) {
 	case 'slugs':
-		$settings_tab = 'wp_lib_slug_group-options';
+		$settings_tab = 'wp_lib_slug_group';
+	break;
+	
+	case 'dash':
+		$settings_tab = 'wp_lib_dash_group';
 	break;
 	
 	default:
-		$settings_tab = 'wp_lib_library_group-options';
+		$settings_tab = 'wp_lib_library_group';
 	break;
 }
 ?>
@@ -51,6 +55,7 @@ switch ( $_GET['tab'] ) {
 	<h2 class="nav-tab-wrapper">
 		<a href="?post_type=wp_lib_items&page=wp-lib-settings" class="nav-tab <?php echo !isset( $_GET['tab'] ) ? 'nav-tab-active' : ''; ?>">General</a>
 		<a href="?post_type=wp_lib_items&page=wp-lib-settings&tab=slugs" class="nav-tab <?php echo ( $_GET['tab'] ) === 'slugs' ? 'nav-tab-active' : ''; ?>">Slugs</a>
+		<a href="?post_type=wp_lib_items&page=wp-lib-settings&tab=dash" class="nav-tab <?php echo ( $_GET['tab'] ) === 'dash' ? 'nav-tab-active' : ''; ?>">Dashboard</a>
     </h2>
 
 	<div id="wp-lib-main-content">
@@ -60,7 +65,7 @@ switch ( $_GET['tab'] ) {
 			settings_fields( $settings_tab );
 			
 			// Renders selected tab's settings fields
-			do_settings_sections( $settings_tab );
+			do_settings_sections( $settings_tab . '-options' );
 			
 			// Submit button to update options
 			submit_button( 'Update', 'primary' );

wp-librarian-functions.php
@@ -310,7 +310,7 @@ function wp_lib_loan_item( $item_id, $member_id, $loan_length = false ) {
 	
 	// If loan length wasn't given, use default loan length
 	if ( !$loan_length )
-		$loan_length = get_option( 'wp_lib_loan_length', 12 );
+		$loan_length = get_option( 'wp_lib_loan_length', array(12) )[0];
 	// If loan length is not a positive integer, call error
 	elseif ( !ctype_digit( $loan_length ) ) {
 		wp_lib_error( 311 );
@@ -327,7 +327,7 @@ function wp_lib_loan_item( $item_id, $member_id, $loan_length = false ) {
 		return $loan_id;
 	
 	// Passes item to member then checks for success
-	if ( !wp_lib_give_item( $item_id, $loan_id, $member_id ) ) {
+	if ( !wp_lib_give_item( $loan_id ) ) {
 		wp_lib_error( 411 );
 		return false;
 	}
@@ -384,9 +384,13 @@ function wp_lib_schedule_loan( $item_id, $member_id, $start_date, $end_date ) {
 }
 
 // Represents the physical passing of the item from Library to Member. Item is registered as outside the library and relevant meta is updated
-function wp_lib_give_item( $item_id, $loan_id, $member_id, $date = false ) {
+function wp_lib_give_item( $loan_id, $date = false ) {
 	// Sets date to current time if not set
 	wp_lib_prep_date( $date );
+	
+	// Fetches item and member IDs from loan meta
+	$item_id = get_post_meta( $loan_id, 'wp_lib_item', true );
+	$member_id = get_post_meta( $loan_id, 'wp_lib_member', true );
 	
 	/* Updates other meta */
 	
@@ -509,7 +513,7 @@ function wp_lib_create_fine( $item_id, $date = false, $return = true ) {
 	$days_late = -$due_in;
 	
 	// Fetches daily charge for a late item
-	$daily_fine = get_option( 'wp_lib_fine_daily' );
+	$daily_fine = get_option( 'wp_lib_fine_daily', array(0) )[0];
 	
 	// Calculates fine based off days late * charge per day
 	$fine = $days_late * $daily_fine;
@@ -522,7 +526,7 @@ function wp_lib_create_fine( $item_id, $date = false, $return = true ) {
 	
 	// Fetches member object from item tax
 	$member_id = get_post_meta( $item_id, 'wp_lib_member', true );
-
+	
 	// Saves information relating to fine to its post meta
 	wp_lib_update_meta( $fine_id,
 		array(
@@ -614,9 +618,10 @@ function wp_lib_error( $error_id, $die = false, $param = 'NULL' ) {
 	// 9xx - Error processing error
 	$all_errors = array(
 		110 => 'DateTime neither positive or negative',
-		111 => 'Unexpected currency position',
 		112 => 'Insufficient permissions',
 		113 => "Can not delete {$param} as it is currently on loan. Please return the item first.",
+		114	=> 'Option does not exist',
+		115	=> 'Field value not found in option',
 		200 => 'Item action not recognised',
 		201 => "No {$param} status known for given value",
 		203 => 'Loan not found in item\'s loan index',

wp-librarian-helpers.php
@@ -4,6 +4,13 @@
  * These are a collection of various useful functions used by WP-Librarian to operate
  */
 
+	/* -- File Management -- */
+
+// Loads file containing requested helper classes. Will handle failure less extremely after greater use of classes
+function wp_lib_add_helper( $file_name ) {
+	require_once( plugin_dir_path(__FILE__) . '/helpers/' . $file_name . '.php' );
+}
+
 	/* -- Sanitising Functions -- */
 
 // Sanitizes phone number
@@ -27,12 +34,22 @@ function wp_lib_sanitize_item_cover( $raw ) {
 		return '';
 }
 
-// Santizes HTML POST data from a checkbox
+// Sanitizes HTML POST data from a checkbox
 function wp_lib_sanitize_checkbox( $raw ) {
 	if ( $raw == 'true' )
 		return true;
 	else
 		return false;
+}
+
+// Sanitizes HTML POST data from checkbox from options page
+function wp_lib_sanitize_option_checkbox( $raw ) {
+	return ( $raw === '3' ? 3 : 2 );
+}
+
+// Converts database checkbox value to boolean
+function wp_lib_prep_boolean_option( $option ) {
+	return ( $option === '3' ? true : false );
 }
 
 // Sanitizes string then checks if it is a valid ISBN, returns sanitized ISBN on success or empty string on failure.
@@ -132,7 +149,7 @@ function wp_lib_valid_item_id( $item_id ) {
 		wp_lib_error( 305, false, $item_id );
 		return false;
 	}
-
+	
 	return true;
 }
 
@@ -249,18 +266,6 @@ function wp_lib_get_object_type( $post_id ) {
 }
 
 	/* -- URLs and Slugs -- */
- 
-// Adds plugin url to front of string (e.g. authors -> library/authors)
-function wp_lib_prefix_url( $option, $slug ) {
-	// Gets main public slug ('wp-librarian' by default, usually something like 'library')
-	$main_slug = get_option( 'wp_lib_main_slug', 'wp-librarian' );
-	
-	// Fetches specific slug e.g. 'authors'
-	$sub_slug = get_option( $option, $slug );
-	
-	// Constructs and returns concatenation of the two slugs
-	return $main_slug . '/' . $sub_slug;
-}
 
 function wp_lib_prep_manage_item_params( $item_id ) {
 	return array(
@@ -449,29 +454,26 @@ function wp_lib_plural( $value, $string, $plural = 's' ) {
 
 // Formats money according to user's preferences
 function wp_lib_format_money( $value, $html_ent = true ) {
-	// Fetches user's preferred currency symbol
-	$symbol = get_option( 'wp_lib_currency_symbol' );
+	// Fetches user's preferred currency symbol and currency position (symbol before or after number)
+	$settings = get_option( 'wp_lib_currency', array('&pound;',2) );
+	
+	// Sets friendly variable names
+	$symbol = $settings[0];
+	$position = wp_lib_prep_boolean_option( $settings[1] );
 	
 	// If output doesn't need to use html entities (e.g. &pound; ), converts to actual characters (e.g. £ )
 	if ( !$html_ent ) {
 		$symbol = html_entity_decode( $symbol );
 	}
 	
-	// Fetches user's preferred currency position (before or after the value)
-	$position = (int)get_option( 'wp_lib_currency_position' );
-	
 	// Ensures number has correct number of decimal places
 	$value = number_format( $value, 2 );
 	
 	// Formats $value with currency symbol at preferred position
-	if ( $position === 2 )
-		$value = $symbol . $value;
-	elseif ( $position === 3 )
-		$value = $value . $symbol;
+	if ( $position )
+		return $value . $symbol; // 0.40EUR
 	else
-		wp_lib_error( 111, true );
-		
-	return $value;
+		return $symbol . $value; // £0.40
 }
 
 	/* -- AJAX -- */
@@ -1049,7 +1051,7 @@ function wp_lib_prep_item_status( $item_id, $no_url = false, $short = false ) {
 // Renders item's tax terms and public meta to the page
 function wp_lib_display_item_meta( $item_id, $item_permalink = true ) {
 	// Fetches default taxonomy spacer
-	$spacer = get_option( 'wp_lib_taxonomy_spacer', ', ' );
+	$spacer = get_option( 'wp_lib_taxonomy_spacer', array(', ') )[0];
 	
 	// If user is librarian (or higher), or if the donor is set to be displayed, fetches item donor
 	// If user isn't a librarian, or there is no listed donor, returns false

wp-librarian-options.php
@@ -11,34 +11,23 @@ $options = array(
 	array(
 		// Loan length in days
 		'key'		=> 'wp_lib_loan_length',
-		'default'	=> 12
+		'default'	=> array(12)
 	),
 	array(
 		// Fine length (per day)
 		'key'		=> 'wp_lib_fine_daily',
-		'default'	=> 0.20
+		'default'	=> array(0.20)
 	),
 	/* -- Slugs -- */
 	/* Sections of site urls used when accessing plugin pages e.g. my-site.com/wp-librarian */
 	array(
-		// The slug for library items
-		'key' 		=> 'wp_lib_main_slug',
-		'default'	=> 'wp-librarian'
-	),
-	array(
-		// The sub-slug for single items e.g. library/item/moby-dick
-		'key'		=> 'wp_lib_single_slug',
-		'default'	=> 'item'
-	),
-	array(
-		// The slug for viewing authors
-		'key' 		=> 'wp_lib_authors_slug',
-		'default'	=> 'authors'
-	),
-	array(
-		// The slug for viewing media types
-		'key' 		=> 'wp_lib_media_type_slug',
-		'default'	=> 'type'
+		'key' 		=> 'wp_lib_slugs',
+		'default'	=> array(
+			'wp-librarian',	// The slug for library items
+			'item', 		// The sub-slug for single items e.g. library/item/moby-dick
+			'author',		// The slug for viewing authors
+			'type'			// The slug for viewing media types
+		)
 	),
 	
 	/* -- Formatting -- */
@@ -46,22 +35,17 @@ $options = array(
 	array(
 		// Whether to create the default media types (Books, DVDs etc. )
 		'key' 		=> 'wp_lib_default_media_types',
-		'default'	=> true
+		'default'	=> array(3)
 	),
 	array(
 		// The separator for item taxonomies (the comma between authors)
 		'key' 		=> 'wp_lib_taxonomy_spacer',
-		'default'	=> ', '
+		'default'	=> array(', ')
 	),
 	array(
-		// Currency Symbol
-		'key' 		=> 'wp_lib_currency_symbol',
-		'default'	=> '&pound;'
-	),
-	array(
-		// Currency position relative to the numerical value (1 - Before: £20, 0 - After: 20£)
-		'key' 		=> 'wp_lib_currency_position',
-		'default'	=> '2'
+		// Currency Symbol and position relative to the numerical value (1 - Before: £20, 0 - After: 20£)
+		'key' 		=> 'wp_lib_currency',
+		'default'	=> array('&pound;',2)
 	),
 	
 	/* -- Dashboard -- */
@@ -71,8 +55,8 @@ $options = array(
 		// Whether to automatically search for an item with the given barcode when the given length is reached
 		'key'		=> 'wp_lib_barcode_config',
 		'default'	=> array(
-			'autoFetch'	=> false,
-			'length'	=> 8
+			false,
+			8
 		)
 	)
 );

commit ec5c8f8ebc0886dc1c8b98a0628e062d361c8ff5

Fixed incorrect settings fetching
Fixed bug introduced in 4cc580d8ddc08973603f826a2ad5d5efe0891ae8 caused
by failed propagation of change to method of settings storage

wp-librarian-ajax.php
@@ -1331,7 +1331,7 @@ function wp_lib_page_scheduling_page() {
 	$start_date = Date( 'Y-m-d', current_time( 'timestamp' ) );
 	
 	// Formats placeholder loan end date (current date + default loan length)
-	$end_date = Date( 'Y-m-d', current_time( 'timestamp' ) + ( get_option( 'wp_lib_loan_length', 12 ) * 24 * 60 * 60) );
+	$end_date = Date( 'Y-m-d', current_time( 'timestamp' ) + ( get_option( 'wp_lib_loan_length', array(12) )[0] * 24 * 60 * 60) );
 	
 	$form = array(
 		wp_lib_prep_nonce( 'Scheduling Item: ' . $item_id ),