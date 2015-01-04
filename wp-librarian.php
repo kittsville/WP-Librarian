kittsville/WP-Librarian

commit 5f454399cb3656882245a0ab84618d5015fb352d

Removed deprecated functions
Removed multiple deprecated functions
Replaced meta box rendering functions with anonymous functions

wp-librarian.php
@@ -610,7 +610,7 @@ function wp_lib_setup_item_meta_box() {
 	add_meta_box(
 		'library_items_meta_box',
 		'Item Details',
-		'wp_lib_render_item_meta_box',
+		function(){require_once (plugin_dir_path(__FILE__) . '/wp-librarian-item-meta-box.php');},
 		'wp_lib_items',
 		'normal',
 		'high'
@@ -622,7 +622,7 @@ function wp_lib_setup_member_meta_box() {
 	add_meta_box(
 		'library_members_meta_box',
 		'Member Details',
-		'wp_lib_render_member_meta_box',
+		function(){require_once (plugin_dir_path(__FILE__) . '/wp-librarian-member-meta-box.php');},
 		'wp_lib_members',
 		'normal',
 		'high'

wp-librarian-functions.php
@@ -754,14 +754,4 @@ function wp_lib_update_user_capabilities( $user_id, $role ) {
 	
 	return;
 }
-
-// Renders item meta box below item description on item creation/editing page
-function wp_lib_render_item_meta_box( $item ) {
-	require_once (plugin_dir_path(__FILE__) . '/wp-librarian-item-meta-box.php');
-}
-
-// Renders member meta box below member name on member creation/editing page
-function wp_lib_render_member_meta_box( $member ) {
-	require_once (plugin_dir_path(__FILE__) . '/wp-librarian-member-meta-box.php');
-}
 ?>

wp-librarian-helpers.php
@@ -106,92 +106,6 @@ function wp_lib_sanitize_donor( $member_id ) {
 
 	/* -- Data Validation Functions -- */
 
-// Checks if item ID is valid
-function wp_lib_valid_item_id( $item_id ) {
-	// Checks if ID is a number
-	if ( !is_numeric( $item_id ) ) {
-		wp_lib_error( 301, false, 'Item' );
-		return false;
-	}
-	
-	// Checks if ID belongs to a published/private library item
-	if ( get_post_type( $item_id ) != 'wp_lib_items' ) {
-		wp_lib_error( 305, false, $item_id );
-		return false;
-	}
-	
-	return true;
-}
-
-// Checks if member ID is valid
-function wp_lib_valid_member_id( $member_id ) {
-	// Checks if item ID exists
-	if ( !$member_id ) {
-		wp_lib_error( 300, false, 'Member' );
-		return false;
-	}
-
-	// Checks if ID is a number
-	if ( !is_numeric( $member_id ) ) {
-		wp_lib_error( 301, false, 'Member' );
-		return false;
-	}
-	
-	// Checks if ID belongs to a published/private library item
-	if ( get_post_type( $member_id ) != 'wp_lib_members' ) {
-		wp_lib_error( 305, false, $member_id );
-		return false;
-	}
-
-	return true;
-}
-
-// Checks if loan ID is valid
-function wp_lib_valid_loan_id( $loan_id ) {
-	// Checks if loan ID exists
-	if ( !$loan_id ) {
-		wp_lib_error( 300, false, 'Loan' );
-		return false;
-	}
-	
-	// Checks if ID is actually a number
-	if ( !is_numeric( $loan_id ) ) {
-		wp_lib_error( 301, false, 'Loan' );
-		return false;
-	}
-	
-	// Checks if ID belongs to a published loan (a loan in any other state is not valid)
-	if ( get_post_type( $loan_id ) != 'wp_lib_loans' ) {
-		wp_lib_error( 306 );
-		return false;
-	}
-
-	return true;
-}
-
-// Checks if fine ID is valid
-function wp_lib_valid_fine_id( $fine_id ){
-	// Checks if fine ID exists
-	if ( !$fine_id ) {
-		wp_lib_error( 300, false, 'Fine' );
-		return false;
-	}
-	
-	// Checks if ID is actually a number
-	if ( !is_numeric( $fine_id ) ) {
-		wp_lib_error( 301, false, 'Fine' );
-		return false;
-	}
-	
-	// Checks if ID belongs to a published loan (a loan in any other state is not valid)
-	if ( get_post_type( $fine_id ) != 'wp_lib_fines' ) {
-		wp_lib_error( 308 );
-		return false;
-	}
-
-	return true;
-}
-
 // Checks if ID belongs to valid Library item, returns object type if true
 function wp_lib_get_object_type( $post_id ) {
 	// Checks if item ID exists

commit 63db95220af41ead59272c3990c189ab7f632772

Fixed item and member meta not displaying
Fixed bug introduced in 5f454399cb3656882245a0ab84618d5015fb352d through
carelessness

wp-librarian.php
@@ -630,7 +630,7 @@ function wp_lib_setup_item_meta_box() {
 	add_meta_box(
 		'library_items_meta_box',
 		'Item Details',
-		function(){require_once (dirname( __FILE__ ) . '/wp-librarian-item-meta-box.php');},
+		function($item){require_once (dirname( __FILE__ ) . '/wp-librarian-item-meta-box.php');},
 		'wp_lib_items',
 		'normal',
 		'high'
@@ -642,7 +642,7 @@ function wp_lib_setup_member_meta_box() {
 	add_meta_box(
 		'library_members_meta_box',
 		'Member Details',
-		function(){require_once (dirname( __FILE__ ) . '/wp-librarian-member-meta-box.php');},
+		function($item){require_once (dirname( __FILE__ ) . '/wp-librarian-member-meta-box.php');},
 		'wp_lib_members',
 		'normal',
 		'high'