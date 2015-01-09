e6168911882c09460964e300861ca6f49acd94da
Kit <kittsville@googlemail.com>
2015-01-09 15:54:20 +0000

    Improved Code readability
    Improved tabbed alignment for greater readability

--- wp-librarian-helpers.php
+++ wp-librarian-helpers.php
@@ -377,8 +377,8 @@ function wp_lib_prep_item_meta_box( $item_id ) {
 	
 	// Item meta fields to be displayed in management header
 	$meta_fields = array(
-		array( 'Item ID', $item_id),
-		array( 'Condition', wp_lib_format_item_condition( $meta['wp_lib_item_condition'][0] ) )
+		array( 'Item ID',	$item_id),
+		array( 'Condition',	wp_lib_format_item_condition( $meta['wp_lib_item_condition'][0] ) )
 	);
 	
 	// If item has a donor and the ID matches an existing member
@@ -436,15 +436,15 @@ function wp_lib_prep_loan_meta_box( $loan_id ) {
 	
 	// Adds basic loan meta fields
 	$meta_fields = array(
-		array( 'Loan ID', $loan_id ),
-		array( 'Item', wp_lib_manage_item_dash_hyperlink( $meta['wp_lib_item'][0] ) ),
-		array( 'Member', wp_lib_manage_member_dash_hyperlink( $meta['wp_lib_member'][0] ) ),
-		array( 'Creator', (get_the_author_meta( 'user_nicename', get_post_field( 'post_author', $loan_id ) ) )?: '[User Deleted]'),
-		array( 'Expected Start', wp_lib_format_unix_timestamp( $meta['wp_lib_start_date'][0] ) ),
-		array( 'Expected End', wp_lib_format_unix_timestamp( $meta['wp_lib_end_date'][0] ) ),
-		array( 'Actual Start', ( isset( $meta['wp_lib_loaned_date'] ) ? wp_lib_format_unix_timestamp( $meta['wp_lib_loaned_date'][0] ) : 'N/A' ) ),
-		array( 'Actual End', ( isset( $meta['wp_lib_returned_date'] ) ? wp_lib_format_unix_timestamp( $meta['wp_lib_returned_date'][0] ) : 'N/A' ) ),
-		array( 'Status', wp_lib_format_loan_status( $meta['wp_lib_status'][0] ) )
+		array( 'Loan ID',			$loan_id ),
+		array( 'Item',				wp_lib_manage_item_dash_hyperlink( $meta['wp_lib_item'][0] ) ),
+		array( 'Member',			wp_lib_manage_member_dash_hyperlink( $meta['wp_lib_member'][0] ) ),
+		array( 'Creator',			(get_the_author_meta( 'user_nicename', get_post_field( 'post_author', $loan_id ) ) )?: '[User Deleted]'),
+		array( 'Expected Start',	wp_lib_format_unix_timestamp( $meta['wp_lib_start_date'][0] ) ),
+		array( 'Expected End',		wp_lib_format_unix_timestamp( $meta['wp_lib_end_date'][0] ) ),
+		array( 'Actual Start',		( isset( $meta['wp_lib_loaned_date'] ) ? wp_lib_format_unix_timestamp( $meta['wp_lib_loaned_date'][0] ) : 'N/A' ) ),
+		array( 'Actual End',		( isset( $meta['wp_lib_returned_date'] ) ? wp_lib_format_unix_timestamp( $meta['wp_lib_returned_date'][0] ) : 'N/A' ) ),
+		array( 'Status',			wp_lib_format_loan_status( $meta['wp_lib_status'][0] ) )
 	);
 	
 	// If fine was incurred for loan, fetch details and add to metabox
@@ -471,14 +471,14 @@ function wp_lib_prep_fine_meta_box( $fine_id ) {
 		'title'		=> 'Details',
 		'classes'	=> 'fine-man',
 		'fields'	=> array(
-			array( 'Fine ID', $fine_id ),
-			array( 'Loan ID', wp_lib_prep_dash_hyperlink( $meta['wp_lib_loan'][0], wp_lib_prep_manage_loan_params( $meta['wp_lib_loan'][0] ) ) ),
-			array( 'Item', wp_lib_manage_item_dash_hyperlink( $meta['wp_lib_item'][0] ) ),
-			array( 'Member', wp_lib_manage_member_dash_hyperlink( $meta['wp_lib_member'][0] ) ),
-			array( 'Creator', (get_the_author_meta( 'user_nicename', get_post_field( 'post_author', $loan_id ) ) )?: '[User Deleted]'),
-			array( 'Amount', wp_lib_format_money( $meta['wp_lib_fine'][0] ) ),
-			array( 'Status', wp_lib_format_fine_status( $meta['wp_lib_status'][0] ) ),
-			array( 'Created', get_the_date( '', $fine_id ) )
+			array( 'Fine ID',	$fine_id ),
+			array( 'Loan ID',	wp_lib_prep_dash_hyperlink( $meta['wp_lib_loan'][0], wp_lib_prep_manage_loan_params( $meta['wp_lib_loan'][0] ) ) ),
+			array( 'Item',		wp_lib_manage_item_dash_hyperlink( $meta['wp_lib_item'][0] ) ),
+			array( 'Member',	wp_lib_manage_member_dash_hyperlink( $meta['wp_lib_member'][0] ) ),
+			array( 'Creator',	(get_the_author_meta( 'user_nicename', get_post_field( 'post_author', $loan_id ) ) )?: '[User Deleted]'),
+			array( 'Amount',	wp_lib_format_money( $meta['wp_lib_fine'][0] ) ),
+			array( 'Status',	wp_lib_format_fine_status( $meta['wp_lib_status'][0] ) ),
+			array( 'Created',	get_the_date( '', $fine_id ) )
 		)
 	);
 }
@@ -490,12 +490,12 @@ function wp_lib_prep_member_meta_box( $member_id ) {
 	
 	// Sets up header's meta fields
 	$meta_fields = array(
-		array( 'Member ID', $member_id ),
-		array( 'Email', $meta['wp_lib_member_email'][0] ),
-		array( 'Phone', $meta['wp_lib_member_phone'][0] ),
-		array( 'Mobile', $meta['wp_lib_member_mobile'][0] ),
-		array( 'Owed', wp_lib_format_money( wp_lib_fetch_member_owed( $member_id ) ) ),
-		array( 'On Loan', wp_lib_prep_members_items_out( $member_id ) )
+		array( 'Member ID',	$member_id ),
+		array( 'Email',		$meta['wp_lib_member_email'][0] ),
+		array( 'Phone',		$meta['wp_lib_member_phone'][0] ),
+		array( 'Mobile',	$meta['wp_lib_member_mobile'][0] ),
+		array( 'Owed',		wp_lib_format_money( wp_lib_fetch_member_owed( $member_id ) ) ),
+		array( 'On Loan',	wp_lib_prep_members_items_out( $member_id ) )
 	);
 	
 	// Finalises and returns management header
@@ -521,8 +521,8 @@ function wp_lib_prep_member_options( $default_option = true ) {
 	}
 	
 	$args = array(
-		'post_type'			=> 'wp_lib_members',
-		'post_status'		=> 'publish'
+		'post_type'		=> 'wp_lib_members',
+		'post_status'	=> 'publish'
 	);
 	
 	// Fetches all, if any, members
@@ -922,12 +922,12 @@ function wp_lib_display_item_meta( $item_id, $item_permalink = true ) {
 	
 	// Creates array of raw item meta
 	$raw_meta = array(
-		array( 'Title',		get_the_title( $item_id )),
-		array( 'Media Type',get_the_terms( $item_id, 'wp_lib_media_type' )),
-		array( 'Author',	get_the_terms( $item_id, 'wp_lib_author' )),
-		array( 'Donor',		$donor ),
-		array( 'ISBN',		get_post_meta( $item_id, 'wp_lib_item_isbn', true )),
-		array( 'Status',	wp_lib_prep_item_status( $item_id ))
+		array( 'Title',			get_the_title( $item_id )),
+		array( 'Media Type',	get_the_terms( $item_id, 'wp_lib_media_type' )),
+		array( 'Author',		get_the_terms( $item_id, 'wp_lib_author' )),
+		array( 'Donor',			$donor ),
+		array( 'ISBN',			get_post_meta( $item_id, 'wp_lib_item_isbn', true )),
+		array( 'Status',		wp_lib_prep_item_status( $item_id ))
 	);
 	
 	// If item title should be a link to manage the item
