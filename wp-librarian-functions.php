kittsville/WP-Librarian

commit be5ea0c890bf8bcd94539fc1199c1aeb38cf4aad

Fixed Loan Index Fault
Fixed wrong nonce being checked on 'scheduling' dash action
Fixed loan indexes being ordered by loan creation, rather than by
start/end date

wp-librarian-functions.php
@@ -114,39 +114,52 @@ function wp_lib_create_loan_index( $item_id ) {
 		)
 	);
 	
-	// Searches post table for all loans of the item
+	// Searches post table for all loans of the item. Note that loans are returned in creation order which isn't necessarily loan start/end order
 	$query = NEW WP_Query( $args );
 	
-	// Iterates through loans
-	while ( $query->have_posts() ) {
-		// Selects current post (loan)
-		$query->the_post();
+	if ( $query->have_posts() ) {
+		// Iterates through loans
+		while ( $query->have_posts() ) {
+			// Selects current post (loan)
+			$query->the_post();
+			
+			// Fetches loan meta
+			$meta = get_post_meta( get_the_ID() );
+			
+			// Sets start date to date item was given to member, falls back to scheduled start date
+			if ( isset( $meta['wp_lib_loaned_date'] ) )
+				$start_date = $meta['wp_lib_loaned_date'][0];
+			else
+				$start_date = $meta['wp_lib_start_date'][0];
+			
+			// Sets end date to date item was returned, falls back to scheduled end date
+			if ( isset( $meta['wp_lib_returned_date'] ) )
+				$end_date = $meta['wp_lib_returned_date'][0];
+			else
+				$end_date = $meta['wp_lib_end_date'][0];
+			
+			// Adds loan index entry
+			$loan_index[] = array(
+				'start'		=> $start_date,
+				'end'		=> $end_date,
+				'loan_id'	=> get_the_ID()
+			);
+		}
 		
-		// Fetches loan meta
-		$meta = get_post_meta( get_the_ID() );
-		
-		// Sets start date to date item was given to member, falls back to scheduled start date
-		if ( isset( $meta['wp_lib_loaned_date'] ) )
-			$start_date = $meta['wp_lib_loaned_date'][0];
-		else
-			$start_date = $meta['wp_lib_start_date'][0];
-		
-		// Sets end date to date item was returned, falls back to scheduled end date
-		if ( isset( $meta['wp_lib_returned_date'] ) )
-			$end_date = $meta['wp_lib_returned_date'][0];
-		else
-			$end_date = $meta['wp_lib_end_date'][0];
-		
-		
-		$loan_index[] = array(
-			'start'		=> $start_date,
-			'end'		=> $end_date,
-			'loan_id'	=> get_the_ID()
-		);
+		// Sorts array by start/end date rather than post creation order, then returns
+		// Thanks to Nightmare's (http://stackoverflow.com/users/1495319/nightmare) answer to a question to sorting multidimensional arrays (http://stackoverflow.com/questions/11288778)
+		return uasort( $loan_index, function( $a, $b ) {
+			if ($a['start'] > $b['start'])
+				return 1;
+			elseif ($a['start'] < $b['start'])
+				return -1;
+			elseif ($a['start'] == $b['start'])
+				return 0;
+		});
+	} else {
+		// If item has loans, return blank array
+		return array();
 	}
-	
-	// Returns output, a blank array if $query->have_posts == false
-	return $loan_index;
 }
 
 // Calculates days until item needs to be returned, returns negative if item is late

wp-librarian-ajax.php
@@ -120,7 +120,7 @@ add_action( 'wp_ajax_wp_lib_action', function() {
 			}
 
 			// Checks if nonce is valid
-			if ( !wp_lib_verify_nonce( 'Managing Item: ' . $item_id ) )
+			if ( !wp_lib_verify_nonce( 'Scheduling Item: ' . $item_id ) )
 				wp_lib_stop_ajax( false );
 			
 			// Attempts to convert given dates to Unix timestamps

commit 2ea98a128f499f4e808205ea29259890f8d980dc

Fixed bug in scheduling system
Fixed critical bug introduced in
be5ea0c890bf8bcd94539fc1199c1aeb38cf4aad allowing conflicting loans to
be scheduled

wp-librarian-functions.php
@@ -170,7 +170,7 @@ function wp_lib_create_loan_index( $item_id ) {
 		
 		// Sorts array by start/end date rather than post creation order, then returns
 		// Thanks to Nightmare's (http://stackoverflow.com/users/1495319) answer to a question on sorting multidimensional arrays (http://stackoverflow.com/questions/11288778)
-		return uasort( $loan_index, function( $a, $b ) {
+		uasort( $loan_index, function( $a, $b ) {
 			if ($a['start'] > $b['start'])
 				return 1;
 			elseif ($a['start'] < $b['start'])
@@ -178,6 +178,9 @@ function wp_lib_create_loan_index( $item_id ) {
 			elseif ($a['start'] == $b['start'])
 				return 0;
 		});
+		
+		// Returns loan index
+		return $loan_index;
 	} else {
 		// If item has never been loaned, return empty array
 		return array();