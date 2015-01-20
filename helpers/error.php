b8a00788ddbf22dea1f56c053c33039f6dc28f4e
Kit <kittsville@googlemail.com>
2015-01-20 18:51:28 +0000

    Created Error Class
    Created error class to handle error generation. Currently limited by
    lack of a central class. Will be improved in time.

---
+++ helpers/error.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ * Holds information about an error that occurred within WP-Librarian such as the error's description and where it occurred
+ */
+class WP_LIB_ERROR {
+	/*
+	 * All valid error codes within WP-Librarian and their descriptions
+	 * Each block of 100 codes is assigned to a category of error, as such:
+	 * 0xx - Reserved, see wp_lib_add_notification()
+	 * 1xx - Core functionality failure
+	 * 2xx - General loan/return systems error
+	 * 3xx - Invalid loan/return parameters
+	 * 4xx - Error loaning/returning item or fining user
+	 * 5xx - AJAX systems error
+	 * 6xx - Debugging Errors
+	 * 8xx - JavaScript Errors, stored client-side
+	 * 9xx - Error processing error
+	 */
+	public static $error_codes = array(
+		110 => 'DateTime neither positive or negative',
+		112 => 'Insufficient permissions',
+		114	=> 'Option does not exist',
+		115	=> 'Field value not found in option',
+		116	=> 'AJAX classes can not be used outside of AJAX requests',
+		201 => 'No \p status known for given value',
+		204 => 'Multiple items have the same barcode',
+		205	=> 'Deletion can not be completed while an item is on loan',
+		206	=> 'Member does not owe the Library money',
+		207	=> 'Unable to cancel fine as it would result in member owing less than nothing',
+		208 => 'An item cannot be renewed unless it is on loan',
+		209 => 'Item has been renewed the maximum number of times allowed',
+		210 => 'Cannot renew item as it would clash with scheduled loan(s)',
+		211 => 'Loan cannot be fulfilled unless it is currently scheduled',
+		301 => '\p ID given is not a number',
+		302 => 'No loans found for that item ID',
+		303	=> 'No \p with given ID exists',
+		307 => 'Given dates result in an impossible or impractical loan',
+		310 => 'Given date not valid',
+		311 => 'Given loan length invalid (not a valid number)',
+		312 => 'Given date(s) failed to validate',
+		313 => 'Fine can not be cancelled if it is already cancelled',
+		314	=> '\p is required and not given',
+		316	=> 'Given member has been archived and cannot be loaned items',
+		317 => 'Given ID does not belong to a valid Library object',
+		318	=> 'Given barcode invalid',
+		319	=> 'No item found with that barcode',
+		320	=> 'Fine payment amount is invalid',
+		321	=> 'Proposed fine payment is greater than amount owed by member',
+		322	=> 'Loan must be scheduled and the start date must have passed to give item to member',
+		323 => 'Item renewal date must be after item\'s current due date',
+		400 => 'Loan creation failed for unknown reason, sorry :/',
+		401 => 'Can\'t loan item between given dates, There may be a conflicting loan',
+		402 => 'Item not on loan (Loan ID not found in item meta)<br/>This can happen if you refresh the page having already returned an item',
+		403 => 'Loan not found (Loan ID found in item meta but no loan found that ID). The item has now been cleaned of all loan meta to attempt to resolve the issue. Refresh the page.',
+		405 => 'Loan is missing due date',
+		406 => 'Item is/was not late on given date, mate',
+		407 => 'Fine creation failed for unknown reasons, sorry :/',
+		409 => 'Loan status reports item is not currently on loan',
+		410 => 'Item can not be returned on given date because it would be late. Please resolve late item or return item at an earlier date',
+		411 => 'A loan was scheduled but an error occurred when giving the item to the user. The item has not been marked as having left the library!',
+		500 => 'Action requested does not exist',
+		502 => 'Requested Dashboard page not found',
+		503	=> 'Nonce failed to verify, try reloading the page',
+		504	=> 'Unknown API request',
+		505	=> 'Object not authorised for deletion',
+		506 => 'Infinite loop detected, request terminated',
+		600	=> 'Unable to schedule debugging loan',
+		601	=> 'Unable to fulfil successfully scheduled debugging loan',
+		901 => 'Error encountered while processing error (error code not a number)',
+		902 => 'Error encountered while processing error ID:\p (error does not exist)'
+	);
+
+	/*
+	 * Creates the error object
+	 * @param	int		$error_code	Error code, a number referring to an error already defined within the class
+	 * @param	mixed	$param		OPTIONAL Additional details required by certain error codes
+	 */
+	function __construct( $error_code, $param = null ) {
+		// Checks if error code is valid and error exists, if not returns error
+		if ( !is_int( $error_code ) )
+			$error_code = 901;
+	
+		// If given error code does not exist, calls 'undefined error code' error
+		if ( !array_key_exists( $error_code, WP_LIB_ERROR::$error_codes ) )
+			$error_code = 902;
+		
+		// Sets up object properties
+		$this->ID			= $error_code;
+		$this->description	= str_replace( '\p', $param, WP_LIB_ERROR::$error_codes[$error_code] );
+		$this->ajax			= (defined('DOING_AJAX') && DOING_AJAX && isset($GLOBALS['wp_lib_ajax']));
+		
+		// If error was called from an AJAX context, add to AJAX notification buffer
+		// Otherwise kill thread execution
+		if ( $this->ajax ) {
+			global $wp_lib_ajax;
+			
+			$wp_lib_ajax->addNotification( $this->description, $error_code );
+		} else {
+			echo "<div class='wp-lib-error error'><p><strong style=\"color: red;\">WP-Librarian Error {$error_code}: {$this->description}</strong></p></div>";
+			die();
+		}
+	}
+}
+?>
--- wp-librarian-functions.php
+++ wp-librarian-functions.php
@@ -717,98 +717,10 @@ function wp_lib_cancel_fine( $fine_id ) {
  * @param int			$error_id	Error that has occurred
  * @param string|array	$param		OPTIONAL Relevant parameters to error to enhance error message (not optional for certain error messages)
  */
-function wp_lib_error( $error_id, $param = 'NULL' ) {
-	// Checks if error code is valid and error exists, if not returns error
-	if ( !is_numeric( $error_id ) ) {
-		wp_lib_error( 901 );
-		return;
-	}
+function wp_lib_error( $error_id, $param = null ) {
+	wp_lib_load_helper('error');
 	
-	/* 
-	 * Array of all error codes and their explanations
-	 * 0xx - Reserved, see wp_lib_add_notification()
-	 * 1xx - Core functionality failure
-	 * 2xx - General loan/return systems error
-	 * 3xx - Invalid loan/return parameters
-	 * 4xx - Error loaning/returning item or fining user
-	 * 5xx - AJAX systems error
-	 * 6xx - Debugging Errors
-	 * 8xx - JavaScript Errors, stored client-side
-	 * 9xx - Error processing error
-	 */
-	$all_errors = array(
-		110 => 'DateTime neither positive or negative',
-		112 => 'Insufficient permissions',
-		114	=> 'Option does not exist',
-		115	=> 'Field value not found in option',
-		116	=> 'AJAX classes can not be used outside of AJAX requests',
-		201 => "No {$param} status known for given value",
-		204 => 'Multiple items have the same barcode',
-		205	=> 'Deletion can not be completed while an item is on loan',
-		206	=> 'Member does not owe the Library money',
-		207	=> 'Unable to cancel fine as it would result in member owing less than nothing',
-		208 => 'An item cannot be renewed unless it is on loan',
-		209 => 'Item has been renewed the maximum number of times allowed',
-		210 => 'Cannot renew item as it would clash with scheduled loan(s)',
-		211 => 'Loan cannot be fulfilled unless it is currently scheduled',
-		301 => "{$param} ID given is not a number",
-		302 => 'No loans found for that item ID',
-		303	=> "No {$param} with given ID exists",
-		307 => 'Given dates result in an impossible or impractical loan',
-		310 => 'Given date not valid',
-		311 => 'Given loan length invalid (not a valid number)',
-		312 => 'Given date(s) failed to validate',
-		313 => 'Fine can not be cancelled if it is already cancelled',
-		314	=> "{$param} is required and not given",
-		316	=> 'Given member has been archived and cannot be loaned items',
-		317 => 'Given ID does not belong to a valid Library object',
-		318	=> 'Given barcode invalid',
-		319	=> 'No item found with that barcode',
-		320	=> 'Fine payment amount is invalid',
-		321	=> 'Proposed fine payment is greater than amount owed by member',
-		322	=> 'Loan must be scheduled and the start date must have passed to give item to member',
-		323 => 'Item renewal date must be after item\'s current due date',
-		400 => 'Loan creation failed for unknown reason, sorry :/',
-		401 => 'Can\'t loan item between given dates, There may be a conflicting loan',
-		402 => 'Item not on loan (Loan ID not found in item meta)<br/>This can happen if you refresh the page having already returned an item',
-		403 => 'Loan not found (Loan ID found in item meta but no loan found that ID). The item has now been cleaned of all loan meta to attempt to resolve the issue. Refresh the page.',
-		405 => 'Loan is missing due date',
-		406 => 'Item is/was not late on given date, mate',
-		407 => 'Fine creation failed for unknown reasons, sorry :/',
-		409 => 'Loan status reports item is not currently on loan',
-		410 => 'Item can not be returned on given date because it would be late. Please resolve late item or return item at an earlier date',
-		411 => 'A loan was scheduled but an error occurred when giving the item to the user. The item has not been marked as having left the library!',
-		500 => "Action requested does not exist",
-		502 => 'Requested Dashboard page not found',
-		503	=> 'Nonce failed to verify, try reloading the page',
-		504	=> 'Unknown API request',
-		505	=> 'Object not authorised for deletion',
-		506 => 'Infinite loop detected, request terminated',
-		600	=> 'Unable to schedule debugging loan',
-		601	=> 'Unable to fulfil successfully scheduled debugging loan',
-		901 => 'Error encountered while processing error (error code not a number)',
-		902 => "Error encountered while processing error ID:{$param} (error does not exist)"
-	);
-	
-	// Checks if error exists, if not returns error
-	if ( !array_key_exists( $error_id, $all_errors ) ) {
-		wp_lib_error( 902, $error_id );
-		return;
-	}
-	
-	// Fetches error explanation from array
-	$error_text = $all_errors[$error_id];
-	
-	// If error was called during an AJAX request, add to AJAX notification buffer
-	// Otherwise render to page and kill thread
-	if ( defined('DOING_AJAX') && DOING_AJAX && isset($GLOBALS['wp_lib_ajax']) ) {
-		global $wp_lib_ajax;
-		
-		$wp_lib_ajax->addNotification( $error_text, $error_id );
-	} else {
-		echo "<div class='wp-lib-error error'><p><strong style=\"color: red;\">WP-Librarian Error {$error_id}: {$error_text}</strong></p></div>";
-		die();
-	}
+	return new WP_LIB_ERROR( $error_id, $param );
 }
 
 /*
