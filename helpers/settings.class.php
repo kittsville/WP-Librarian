7bcdc0d48cbc229274dc0c2d97dec6f5d6ddd270
Kit <kittsville@googlemail.com>
2015-01-31 23:30:47 +0000

    Improved commenting of settings class

--- helpers/settings.class.php
+++ helpers/settings.class.php
@@ -92,23 +92,28 @@ class WP_LIB_SETTINGS {
 		)
 	);
 	
-	// Iterates over array of settings controlled by WP-Librarian and adds them to the settings database
+	/**
+	 * Adds all plugin default options. If a setting already exists it is ignored
+	 */
 	public function addPluginSettings() {
 		foreach ( self::$plugin_settings as $setting ) {
 			add_option( $setting['key'], $setting['default'] );
 		}
 	}
 	
-	// Iterates over array of settings controlled by WP-Librarian and deletes them
-	// Calling this outside of a situation where the plugin is subsequently deactivated would be unwise
+	/**
+	 * Deletes all plugin options, as specified in $plugin_settings
+	 * As the plugin relies on these options to function, be wary when using this function
+	 */
 	public function purgePluginSettings() {
 		foreach ( self::$plugin_settings as $setting ) {
 			delete_option( $setting['key'], $setting['default'] );
 		}
 	}
 	
-	// Iterates over array of settings controlled by WP-Librarian and ensures they exist and are valid
-	// Used after settings have been updated
+	/**
+	 * Checks if all options controlled by plugin exist. Creates option if one does not
+	 */
 	public function checkPluginSettingsIntegrity() {
 		foreach ( self::$plugin_settings as $setting ) {
 			$setting_value = get_option( $setting['key'], false );
@@ -121,8 +126,9 @@ class WP_LIB_SETTINGS {
 	}
 	
 	/**
-	 * Checks if given key is a valid WP-Librarian settings key
-	 * @param string $settings_key WordPress settings key
+	 * Checks if given options key is one controlled by this plugin
+	 * @param	string	$settings_key	WordPress settings key
+	 * @return	bool					If the option key matched a plugin key
 	 */
 	public function isValidSettingKey( $settings_key ) {
 		foreach ( self::$plugin_settings as $setting ) {
@@ -210,14 +216,21 @@ class WP_LIB_SETTINGS_SECTION extends WP_LIB_SETTINGS {
 		}
 	}
 	
-	// Adds field description to settings field's output
-	// e.g. Set symbol used to denote currency
+	/**
+	 * If settings field has a description field specified, formats as paragraph
+	 * @param	array	$output	An array of strings of HTML
+	 * @param	array	$args	Settings field arguments
+	 */
 	private function addDescription( array &$output, array $args ) {
 		if ( isset( $args['alt'] ) )
 			$output[] = '<p class="tooltip description">' . $args['alt'] . '</p>';
 	}
 	
-	// Fetches option and applies any given filters to it
+	/**
+	 * Gets WordPress option, selects settings field then applies any given filters to it
+	 * @param	array $args	Settings field arguments
+	 * @return	mixed		Settings field's value
+	 */
 	private function getOption( array $args ) {
 		// Fetches option value from database. Uses false if option does not exist
 		$option = get_option( $args['setting_name'], false );
@@ -241,7 +254,12 @@ class WP_LIB_SETTINGS_SECTION extends WP_LIB_SETTINGS {
 		return $option;
 	}
 	
-	// Sets up field's element's properties
+	/**
+	 * Combines given properties with default settings field properties and formats as HTML element properties
+	 * @param	array	$args		Settings field arguments
+	 * @param	array	$add_prop	Additional properties for the settings field input/select element
+	 * @return	string				Settings field HTML element properties
+	 */
 	private function setupFieldProperties( array $args, array $add_prop ) {
 		// Merges given field classes with default field classes
 		$classes = array_merge(
@@ -272,8 +290,10 @@ class WP_LIB_SETTINGS_SECTION extends WP_LIB_SETTINGS {
 		return $properties;
 	}
 	
-	// A type of settings field
-	// Renders an HTML text input (a single line text field)
+	/**
+	 * Renders an HTML text input field
+	 * @param	array	$args	Settings field arguments
+	 */
 	public function textInput( array $args ) {
 		$properties = array(
 			'type' => 'text',
@@ -296,8 +316,10 @@ class WP_LIB_SETTINGS_SECTION extends WP_LIB_SETTINGS {
 		$this->outputLines( $output );
 	}
 	
-	// A type of settings field
-	// Renders an HTML checkbox
+	/**
+	 * Renders an HTML checkbox input
+	 * @param	array	$args	Settings field arguments
+	 */
 	public function checkboxInput( array $args ) {
 		$properties = array(
 			'type'	=> 'checkbox',
@@ -323,8 +345,10 @@ class WP_LIB_SETTINGS_SECTION extends WP_LIB_SETTINGS {
 		$this->outputLines( $output );
 	}
 	
-	// Renders field's HTML
-	// @param array $lines Strings of HTML to be echoed
+	/**
+	 * Echoes an array of strings of HTML
+	 * @param	array	$lines	An array of strings of HTML
+	 */
 	private function outputLines( array $lines ) {
 		foreach ( $lines as $line ) {
 			echo $line;
