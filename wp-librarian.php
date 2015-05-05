kittsville/WP-Librarian

commit 8bdce3aa34b13ba72c3a2e1a544c4a8be61ea79d

Removed variable declaration
The variable was never used and might have encouraged naughty developers
to use it

wp-librarian.php
@@ -32,7 +32,7 @@ define('WP_LIB_MAINTAIN_INTEGRITY', true);
 
 require_once (dirname( __FILE__ ) . '/lib/wp-librarian.class.php');
 
-$plugin = new WP_LIBRARIAN;
+new WP_LIBRARIAN;
 
 register_activation_hook(__FILE__, array($plugin, 'runOnActivation'));
 

commit df62920c93569a5deeff364fea090fa1f5a8f69e

Fixed activation bug
Fixed bug introduced in 8bdce3aa34b13ba72c3a2e1a544c4a8be61ea79d by
necessary variable being removed

wp-librarian.php
@@ -32,8 +32,8 @@ define('WP_LIB_MAINTAIN_INTEGRITY', true);
 
 require_once (dirname( __FILE__ ) . '/lib/wp-librarian.class.php');
 
-new WP_LIBRARIAN;
+$wp_librarian = new WP_LIBRARIAN;
 
-register_activation_hook(__FILE__, array($plugin, 'runOnActivation'));
+register_activation_hook(__FILE__, array($wp_librarian, 'runOnActivation'));
 
 register_deactivation_hook(__FILE__, 'flush_rewrite_rules');