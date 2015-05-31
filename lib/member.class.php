kittsville/WP-Librarian

commit 4e593c8736fa51ce5720bf2056f9ee7a26e7a962

Removed unintended pagnation of queries

lib/member.class.php
@@ -26,6 +26,8 @@ class WP_LIB_MEMBER extends WP_LIB_OBJECT {
 		// Queries WP for all loans to current member where a fine was incurred
 		$query = new WP_Query(array(
 			'post_type' 		=> 'wp_lib_loans',
+			'post_status',		=> 'published',
+			'nopaging'			=> true,
 			'meta_query'		=> array(
 				'relation'		=> 'AND',
 				array(

helpers/ajax.class.php
@@ -519,6 +519,7 @@ class WP_LIB_AJAX_ACTION extends WP_LIB_AJAX {
 			array(
 				'post_type'     => 'wp_lib_members',
 				'post_status'   => 'publish',
+				'nopaging'      => true,
 				'meta_query'    => array(
 					array(
 						'key'       => 'wp_lib_member_archive',
@@ -1146,7 +1147,7 @@ class WP_LIB_AJAX_PAGE extends WP_LIB_AJAX {
 			array(
 				'post_type'         => 'wp_lib_items',
 				'post_status'       => 'publish',
-				'posts_per_page'    => '-1'
+				'nopaging'          => true
 			)
 		);
 		
@@ -2432,6 +2433,7 @@ class WP_LIB_AJAX_API extends WP_LIB_AJAX {
 		$query = new WP_Query(array(
 			'post_type'     => 'wp_lib_items',
 			'post_status'   => 'publish',
+			'nopaging'      => true
 			'meta_query'    => array(
 				$meta_query
 			)

lib/admin-tables.class.php
@@ -254,6 +254,7 @@ class WP_LIB_ADMIN_TABLES {
 			case 'member_donated':
 				echo (new WP_Query(array(
 					'post_type' 		=> 'wp_lib_items',
+					'nopaging'			=> true,
 					'meta_query'		=> array(
 						array(
 							'key'			=> 'wp_lib_item_donor',

lib/item.class.php
@@ -128,6 +128,7 @@ class WP_LIB_ITEM extends WP_LIB_OBJECT {
 		$query = NEW WP_Query(array(
 			'post_type'		=> 'wp_lib_loans',
 			'post_status'	=> 'publish',
+			'nopaging'		=> true,
 			'meta_query'	=> array(
 				array(
 					'key'		=> 'wp_lib_item',

lib/wp-librarian.class.php
@@ -148,6 +148,8 @@ class WP_LIBRARIAN {
 				// Deletes member post meta 'wp_lib_owed'
 				$members_query = new WP_Query(array(
 					'post_type'         => 'wp_lib_members',
+					'post_status'		=> 'publish',
+					'nopaging'      	=> true,
 					'meta_query'        => array(
 						array(
 							'key'           => 'wp_lib_owed',
@@ -166,7 +168,9 @@ class WP_LIBRARIAN {
 				
 				// Renames fine post meta 'wp_lib_fine' to 'wp_lib_owed'
 				$fines_query = new WP_Query(array(
-					'post_type' => 'wp_lib_fines',
+					'post_type'		=> 'wp_lib_fines',
+					'post_status'	=> 'publish',
+					'nopaging'      => true
 				));
 				
 				if ($fines_query->have_posts()){

commit b14a2ba806b1a7cbbefe34dbc1365bef8e46af1c

Fixed syntax error
Fixed error introduced in 4e593c8736fa51ce5720bf2056f9ee7a26e7a962

lib/member.class.php
@@ -26,7 +26,7 @@ class WP_LIB_MEMBER extends WP_LIB_OBJECT {
 		// Queries WP for all loans to current member where a fine was incurred
 		$query = new WP_Query(array(
 			'post_type' 		=> 'wp_lib_loans',
-			'post_status',		=> 'published',
+			'post_status'		=> 'published',
 			'nopaging'			=> true,
 			'meta_query'		=> array(
 				'relation'		=> 'AND',