kittsville/WP-Librarian

commit 8ef5b00ad125ea14387ef308fc73d212b55b03ee

Added post-fine creation action
Documentation has already been written
[here](/kittsville/WP-Librarian/wiki/wp_lib_fine_created)

lib/loan.class.php
@@ -308,6 +308,8 @@ class WP_Lib_Loan extends WP_Lib_Object {
 					
 					// Saves fine ID to loan meta
 					add_post_meta($this->ID, 'wp_lib_fine', $fine_id);
+					
+					do_action('wp_lib_fine_created', $fine_id, $item_id, $member_id, $loan),
 				break;
 				
 				// If fine has been wavered, allow late return with no fine

commit 1362fe8070930997bf98a5517dfd2bace4021216

Fixed syntax error in loan class
Introduced in 8ef5b00ad125ea14387ef308fc73d212b55b03ee

lib/loan.class.php
@@ -309,7 +309,7 @@ class WP_Lib_Loan extends WP_Lib_Object {
 					// Saves fine ID to loan meta
 					add_post_meta($this->ID, 'wp_lib_fine', $fine_id);
 					
-					do_action('wp_lib_fine_created', $fine_id, $item_id, $member_id, $loan),
+					do_action('wp_lib_fine_created', $fine_id, $item_id, $member_id, $loan);
 				break;
 				
 				// If fine has been wavered, allow late return with no fine