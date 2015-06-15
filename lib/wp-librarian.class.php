kittsville/WP-Librarian

commit b854481bfae1453677dfa9a5b3f9f20b46202eb8

Improved handling of currency
Replaced handling of currency as a float to handling as an int * 100
Fixed bug that reported money still owed if payment resulted in negative
money owed (which is legal)
Added migrations to update all <0.3 systems payments and fine amounts to
new currency representation

lib/wp-librarian.class.php
@@ -140,7 +140,7 @@ class WP_Librarian {
 		
 		// If plugin has been previously installed
 		if (is_array($version)) {
-			// If previous plugin version is version 0.1 or older, remove depreciated data
+			// If previous plugin version is version 0.1 or earlier, remove depreciated data
 			if (version_compare($version['version'], '0.1', '<=')) {
 				delete_option('wp_lib_default_media_types');
 				delete_option('wp_lib_taxonomy_spacer');
@@ -184,6 +184,51 @@ class WP_Librarian {
 						delete_post_meta($fine_id, 'wp_lib_fine');
 						
 						update_post_meta($fine_id, 'wp_lib_owed', $fine_amount);
+					}
+				}
+			}
+			
+			/**
+			 * If previous plugin version is version 0.2 or earlier, update handling of currency by:
+			 * 1. Updating fine payments stored in member meta
+			 * 2. Updating fine amounts stored in fine meta
+			 */
+			if (version_compare($version['version'], '0.2', '<=')) {
+				$members = new WP_Query(array(
+					'post_type'     => 'wp_lib_members',
+					'post_status'   => 'publish',
+					'nopaging'      => true
+				));
+				
+				if ($members->have_posts()) {
+					$member_ids = wp_list_pluck($members->posts, 'ID');
+					
+					foreach ($member_ids as $member_id) {
+						$fine_payments = get_post_meta($member_id, 'wp_lib_payments');
+						
+						delete_post_meta($member_id, 'wp_lib_payments');
+						
+						foreach ($fine_payments as $fine_payment) {
+							$fine_payment[1] = intval($fine_payment[1] * 100);
+							
+							add_post_meta($member_id, 'wp_lib_payments', $fine_payment);
+						}
+					}
+				}
+				
+				$fines = new WP_Query(array(
+					'post_type'     => 'wp_lib_fines',
+					'post_status'   => 'publish',
+					'nopaging'      => true
+				));
+				
+				if ($fines->have_posts()) {
+					$fine_ids = wp_list_pluck($fines->posts, 'ID');
+					
+					foreach ($fine_ids as $fine_id) {
+						$fine_amount = get_post_meta($fine_id, 'wp_lib_owed', true);
+						
+						update_post_meta($fine_id, intval($fine_amount * 100));
 					}
 				}
 			}

helpers/ajax.class.php
@@ -598,9 +598,9 @@ class WP_Lib_AJAX_Action extends WP_Lib_AJAX {
 	 * @todo Move payment functionality to functions.php, actions should only prepare data and call functions
 	 */
 	private function doPayFine() {
-		// Fetches member and fine amount
+		// Fetches member and fine amount, converting fine amount to an integer
 		$member         = $this->getMember();
-		$fine_payment   = floatval(isset($_POST['fine_payment']) ? $_POST['fine_payment'] : 0);
+		$fine_payment   = intval((isset($_POST['fine_payment']) ? $_POST['fine_payment'] : 0) * 100);
 		
 		// Validates Nonce
 		$this->checkNonce('Pay Member Fines ' . $member->ID);
@@ -619,7 +619,7 @@ class WP_Lib_AJAX_Action extends WP_Lib_AJAX {
 		$owed = $member->getMoneyOwed();
 		
 		// If money is still owed by the member, inform librarian
-		if ($owed != 0)
+		if ($owed > 0)
 			$notification .= ' ' . wp_lib_format_money($owed) . ' is still owed.';
 		
 		$this->endAction(true, $notification);

lib/loan.class.php
@@ -286,8 +286,8 @@ class WP_Lib_Loan extends WP_Lib_Object {
 					// Due in -5 days == 5 days late
 					$days_late = -$due_in;
 					
-					// Fetches daily charge for a late item
-					$daily_fine = get_option('wp_lib_fine_daily', array(0))[0];
+					// Fetches daily charge for a late item (as an int)
+					$daily_fine = intval(get_option('wp_lib_fine_daily', array(0))[0] * 100);
 					
 					// Calculates fine based off days late * charge per day
 					$fine_amount = $days_late * $daily_fine;

lib/member.class.php
@@ -19,8 +19,8 @@ class WP_Lib_Member extends WP_Lib_Object {
 	}
 	
 	/**
-	 * Calculates amount currently owed by member in late fines
-	 * @return  float|int   Amount currently owed by member without currency symbol
+	 * Calculates amount currently owed by member in late fines * 100
+	 * @return  int   Amount currently owed by member without currency symbol
 	 */
 	public function getMoneyOwed() {
 		// Queries WP for all loans to current member where a fine was incurred
@@ -73,8 +73,8 @@ class WP_Lib_Member extends WP_Lib_Object {
 	/**
 	 * Adds fine payment to member records
 	 * Fine payments that result in a negative amount owed are allowed
-	 * @param   float|int           $payment    Amount to be paid
-	 * @return  bool|WP_Lib_Error               True on success, error on failure
+	 * @param   int           		$payment	Amount to be paid x 100 e.g. £1.20 == 120
+	 * @return  bool|WP_Lib_Error   	     	True on success, error on failure
 	 */
 	public function payMoneyOwed($payment) {
 		// If fine payment is negative or failed to validate (resulting in 0), call error

wp-librarian-helpers.php
@@ -334,6 +334,9 @@ function wp_lib_format_money($value, $html_ent = true) {
 	// For the silly event that a fine of nothing managed to get charged
 	$value = $value == '' ? 0 : $value;
 	
+	// Converts to float (currency is handled as an int)
+	$value = $value / 100;
+	
 	// Ensures number has correct number of decimal places
 	$value = number_format($value, 2);
 	

commit 2c5f7d4b9785f7578af782ef24f96733a51cf7cb

Fixed issue with currency migration
Introduced in b854481bfae1453677dfa9a5b3f9f20b46202eb8
#49

lib/wp-librarian.class.php
@@ -226,9 +226,9 @@ class WP_Librarian {
 					$fine_ids = wp_list_pluck($fines->posts, 'ID');
 					
 					foreach ($fine_ids as $fine_id) {
-						$fine_amount = get_post_meta($fine_id, 'wp_lib_owed', true);
+						$fine_amount = floatval(get_post_meta($fine_id, 'wp_lib_owed', true));
 						
-						update_post_meta($fine_id, intval($fine_amount * 100));
+						update_post_meta($fine_id, 'wp_lib_owed', intval($fine_amount * 100));
 					}
 				}
 			}